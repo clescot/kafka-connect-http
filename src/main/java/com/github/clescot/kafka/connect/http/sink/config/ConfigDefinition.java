71d66e3620cf8933cb57d5e1c21ca6a97013d381
Charles  Lescot <charles.lescot-externe@icade.fr>
2022-10-28 22:14:22 +0200

    init WsSourceConnector #2

--- src/main/java/com/github/clescot/kafka/connect/http/sink/config/ConfigDefinition.java
+++ src/main/java/com/github/clescot/kafka/connect/http/sink/config/ConfigDefinition.java
@@ -13,11 +13,6 @@ public class ConfigDefinition {
 
     public static ConfigDef config() {
         return new ConfigDef()
-                .define(ConfigConstants.TARGET_BOOTSTRAP_SERVER, ConfigDef.Type.STRING, null, ConfigDef.Importance.HIGH, ConfigConstants.TARGET_BOOTSTRAP_SERVER_DOC)
-                .define(ConfigConstants.TARGET_SCHEMA_REGISTRY, ConfigDef.Type.STRING, null, ConfigDef.Importance.HIGH, ConfigConstants.TARGET_SCHEMA_REGISTRY_DOC)
-                .define(ConfigConstants.ACK_TOPIC, ConfigDef.Type.STRING, null, ConfigDef.Importance.HIGH, ConfigConstants.ACK_TOPIC_DOC)
-                .define(ConfigConstants.ACK_SCHEMA, ConfigDef.Type.STRING, null, ConfigDef.Importance.HIGH, ConfigConstants.ACK_SCHEMA_DOC)
-                .define(ConfigConstants.VALUE_CONVERTER, ConfigDef.Type.STRING, StringConverter.class.getName(), ConfigDef.Importance.HIGH, ConfigConstants.VALUE_CONVERTER_DOC)
-                .define(ConfigConstants.PRODUCER_CLIENT_ID, ConfigDef.Type.STRING, DEFAULT_PRODUCER_CLIENT_ID, ConfigDef.Importance.LOW, ConfigConstants.PRODUCER_CLIENT_ID_DOC);
+                .define(ConfigConstants.ACK_TOPIC, ConfigDef.Type.STRING, null, ConfigDef.Importance.HIGH, ConfigConstants.ACK_TOPIC_DOC);
     }
 }
