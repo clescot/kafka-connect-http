cb59d6c4a4be69c4bbcceb345ea51ee443b091d6
Charles  Lescot <charles.lescot-externe@icade.fr>
2022-11-01 02:54:57 +0100

    ITConnectorTest.java mock http server ok. send kakfa record into http-requests not according to the JSON Schema (need to be set into the SR)

--- src/test/java/com/github/clescot/kafka/connect/http/ITConnectorTest.java
+++ src/test/java/com/github/clescot/kafka/connect/http/ITConnectorTest.java
@@ -1,12 +1,27 @@
 package com.github.clescot.kafka.connect.http;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.awaitility.Awaitility.await;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import com.google.common.base.Joiner;
 import io.debezium.testing.testcontainers.Connector;
 import io.debezium.testing.testcontainers.ConnectorConfiguration;
 import io.debezium.testing.testcontainers.DebeziumContainer;
 import io.debezium.testing.testcontainers.SchemaRegistryContainer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.rnorth.ducttape.unreliables.Unreliables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.KafkaContainer;
@@ -16,12 +31,15 @@ import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.lifecycle.Startables;
-import org.testcontainers.shaded.org.awaitility.Awaitility;
 import org.testcontainers.utility.DockerImageName;
 
 import java.io.*;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.jar.Attributes;
 import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
@@ -29,6 +47,7 @@ import java.util.jar.Manifest;
 import java.util.stream.Stream;
 
 @Testcontainers
+@WireMockTest
 public class ITConnectorTest {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(ITConnectorTest.class);
@@ -43,7 +62,7 @@ public class ITConnectorTest {
     private static final SchemaRegistryContainer schemaRegistryContainer = new SchemaRegistryContainer()
             .withNetwork(network)
             .withKafka(kafkaContainer)
-//            .withLogConsumer(new Slf4jLogConsumer(LOGGER))
+            .withLogConsumer(new Slf4jLogConsumer(LOGGER))
             .dependsOn(kafkaContainer)
             .withStartupTimeout(Duration.ofSeconds(90));
 
@@ -51,7 +70,7 @@ public class ITConnectorTest {
                     .withFileSystemBind("target/http-connector", "/usr/local/share/kafka/plugins")
                     .withLogConsumer(new Slf4jLogConsumer(LOGGER))
                     .withNetwork(network)
-//                    .withKafka(kafkaContainer)
+                    .withKafka(kafkaContainer)
                     .withEnv("CONNECT_BOOTSTRAP_SERVERS",kafkaContainer.getNetworkAliases().get(0) + ":9092")
                     .withEnv("CONNECT_GROUP_ID","test")
                     .withEnv("CONNECT_CONFIG_STORAGE_TOPIC","test_config")
@@ -68,16 +87,9 @@ public class ITConnectorTest {
                             "org.apache.kafka.connect=ERROR," +
                             "org.apache.kafka.connect.runtime.distributed=ERROR," +
                             "org.apache.kafka.connect.runtime.isolation=DEBUG," +
+                            "org.reflections=ERROR," +
                             "org.apache.kafka.clients=ERROR")
                     .withEnv("CONNECT_PLUGIN_PATH","/usr/share/java/,/usr/share/confluent-hub-components/,/usr/local/share/kafka/plugins")
-//                    .withEnv("CONNECT_LOG4J_ROOT_LOGLEVEL","INFO")
-//                    .withEnv("CONNECT_LOG4J_LOGGERS","org.apache.kafka.connect=DEBUG")
-//                    .withEnv("GROUP_ID", "1")
-//                    .withEnv("CONFIG_STORAGE_TOPIC", "debezium_connect_config")
-//                    .withEnv("OFFSET_STORAGE_TOPIC", "debezium_connect_offsets")
-//                    .withEnv("STATUS_STORAGE_TOPIC", "debezium_connect_status")
-//                    .withEnv("CONNECT_KEY_CONVERTER_SCHEMAS_ENABLE", "false")
-//                    .withEnv("CONNECT_VALUE_CONVERTER_SCHEMAS_ENABLE", "false")
                     .withExposedPorts(8083)
                     .dependsOn(kafkaContainer,schemaRegistryContainer)
                     .waitingFor(Wait.forHttp("/connector-plugins/"));
@@ -85,67 +97,86 @@ public class ITConnectorTest {
 
     @BeforeAll
     public static void startContainers() throws IOException {
-//        createConnectorJar("target/kafka-connect-http-sink-for-it.jar");
-        Startables.deepStart(Stream.of(
-                        kafkaContainer,schemaRegistryContainer,connectContainer))
-                .join();
+        Startables.deepStart(Stream.of(kafkaContainer,schemaRegistryContainer,connectContainer)).join();
     }
 
     @Test
-    public void nominalCase(){
-        ConnectorConfiguration connector = ConnectorConfiguration.create()
+    public void nominalCase(WireMockRuntimeInfo wmRuntimeInfo){
+        ConnectorConfiguration sinkConnectorConfiguration = ConnectorConfiguration.create()
                 .with("connector.class", "com.github.clescot.kafka.connect.http.sink.WsSinkConnector")
                 .with("tasks.max", "2")
-                .with("topics", "test,toto")
+                .with("topics", "http-requests")
+                .with("key.converter", "org.apache.kafka.connect.storage.StringConverter")
+                .with("value.converter", "io.confluent.connect.json.JsonSchemaConverter")
+                .with("value.converter.schema.registry.url","http://"+schemaRegistryContainer.getHost()+schemaRegistryContainer.getMappedPort(8081));
+        ConnectorConfiguration sourceConnectorConfiguration = ConnectorConfiguration.create()
+                .with("connector.class", "com.github.clescot.kafka.connect.http.source.WsSourceConnector")
+                .with("tasks.max", "2")
+                .with("topics", "http-responses")
                 .with("key.converter", "org.apache.kafka.connect.storage.StringConverter")
                 .with("value.converter", "org.apache.kafka.connect.storage.StringConverter");
 
-        connectContainer.registerConnector("http-connector", connector);
+        connectContainer.registerConnector("http-connector", sinkConnectorConfiguration);
         connectContainer.ensureConnectorTaskState("http-connector", 0, Connector.State.RUNNING);
 
         List<String> registeredConnectors = connectContainer.getRegisteredConnectors();
 
         String joinedRegisteredConnectors = Joiner.on(",").join(registeredConnectors);
         LOGGER.info("registered connectors :{}",joinedRegisteredConnectors);
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!:"+joinedRegisteredConnectors);
+        String httpBaseUrl = wmRuntimeInfo.getHttpBaseUrl();
+        LOGGER.info("wiremock : {}",httpBaseUrl);
+        WireMock wireMock = wmRuntimeInfo.getWireMock();
+        wireMock.register(get("/ping").willReturn(aResponse().withBody("pong").withStatus(200).withStatusMessage("OK")));
+        KafkaProducer<String, String> producer = getProducer(kafkaContainer);
+        ProducerRecord<String,String> record = new ProducerRecord<>("http-requests","value");
+        producer.send(record);
+        producer.flush();
+        await().atMost(Duration.ofSeconds(1000)).until(()->Boolean.TRUE.equals(Boolean.FALSE));
     }
 
-    private static void createConnectorJar(String jarPath) throws IOException {
-        Manifest manifest = new Manifest();
-        manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
-        JarOutputStream target = new JarOutputStream(new FileOutputStream(jarPath), manifest);
-        add(new File("target/classes"), target);
-        target.close();
+
+    private KafkaProducer<String, String> getProducer(
+            KafkaContainer kafkaContainer) {
+
+        return new KafkaProducer<>(
+                Map.of(
+                        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                        kafkaContainer.getBootstrapServers()
+                        ),
+                new StringSerializer(),
+                new StringSerializer());
     }
 
-    private static void add(File source, JarOutputStream target) throws IOException {
-        String name = source.getPath().replace("\\", "/").replace("target/classes/", "");
-        if (source.isDirectory()) {
-            if (!name.endsWith("/")) {
-                name += "/";
-            }
-            JarEntry entry = new JarEntry(name);
-            entry.setTime(source.lastModified());
-            target.putNextEntry(entry);
-            target.closeEntry();
-            for (File nestedFile : source.listFiles()) {
-                add(nestedFile, target);
-            }
-        }
-        else {
-            JarEntry entry = new JarEntry(name);
-            entry.setTime(source.lastModified());
-            target.putNextEntry(entry);
-            try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(source))) {
-                byte[] buffer = new byte[1024];
-                while (true) {
-                    int count = in.read(buffer);
-                    if (count == -1)
-                        break;
-                    target.write(buffer, 0, count);
-                }
-                target.closeEntry();
-            }
-        }
+    private KafkaConsumer<String, String> getConsumer(
+            KafkaContainer kafkaContainer) {
+
+        return new KafkaConsumer<>(
+                Map.of(
+                        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                        kafkaContainer.getBootstrapServers(),
+                        ConsumerConfig.GROUP_ID_CONFIG,
+                        "test-" + UUID.randomUUID(),
+                        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+                        "earliest"),
+                new StringDeserializer(),
+                new StringDeserializer());
     }
+
+    private List<ConsumerRecord<String, String>> drain(
+            KafkaConsumer<String, String> consumer,
+            int expectedRecordCount) {
+
+        List<ConsumerRecord<String, String>> allRecords = new ArrayList<>();
+
+        Unreliables.retryUntilTrue(10, TimeUnit.SECONDS, () -> {
+            consumer.poll(Duration.ofMillis(50))
+                    .iterator()
+                    .forEachRemaining(allRecords::add);
+
+            return allRecords.size() == expectedRecordCount;
+        });
+
+        return allRecords;
+    }
+
 }
