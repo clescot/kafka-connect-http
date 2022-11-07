3c5248a770c99be938aafc1769492ba7de92aef5
Charles Lescot <910188+clescot@users.noreply.github.com>
2022-11-07 19:51:24 +0100

    fix concurrent test

--- config/debug.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd ..
-mvn clean package -DskipTests
-export CLASSPATH="$(find target/ -type f -name '*.jar' | tr '\n' ':')"
-export KAFKA_JMX_OPTS="-Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=${SUSPEND},address=5005"
-docker run -v ${pwd}:/config confluentinc/cp-kafka-connect:5.3.1   connect-standalone   /config/connect-avro-docker.properties /config/sink-connector.properties
