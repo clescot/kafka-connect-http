076a2d32e973edbb5b8c620f150c497b1cb6d06d
Charles Lescot <910188+clescot@users.noreply.github.com>
2022-11-07 19:51:22 +0100

    add key.value config

--- src/main/java/com/github/clescot/kafka/connect/http/sink/config/ConfigDefinition.java
+++ src/main/java/com/github/clescot/kafka/connect/http/sink/config/ConfigDefinition.java
@@ -1,8 +1,11 @@
 package com.github.clescot.kafka.connect.http.sink.config;
 
 import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.storage.StringConverter;
 
 public class ConfigDefinition {
+
+    public static final String DEFAULT_PRODUCER_CLIENT_ID = "httpSinkProducer";
 
     private ConfigDefinition() {
         //Class with only static methods
@@ -14,6 +17,7 @@ public class ConfigDefinition {
                 .define(ConfigConstants.TARGET_SCHEMA_REGISTRY, ConfigDef.Type.STRING, null, ConfigDef.Importance.HIGH, ConfigConstants.TARGET_SCHEMA_REGISTRY_DOC)
                 .define(ConfigConstants.ACK_TOPIC, ConfigDef.Type.STRING, null, ConfigDef.Importance.HIGH, ConfigConstants.ACK_TOPIC_DOC)
                 .define(ConfigConstants.ACK_SCHEMA, ConfigDef.Type.STRING, null, ConfigDef.Importance.HIGH, ConfigConstants.ACK_SCHEMA_DOC)
-                .define(ConfigConstants.PRODUCER_CLIENT_ID, ConfigDef.Type.STRING, "sinkProducer", ConfigDef.Importance.LOW, ConfigConstants.PRODUCER_CLIENT_ID_DOC);
+                .define(ConfigConstants.VALUE_CONVERTER, ConfigDef.Type.STRING, StringConverter.class.getName(), ConfigDef.Importance.HIGH, ConfigConstants.VALUE_CONVERTER_DOC)
+                .define(ConfigConstants.PRODUCER_CLIENT_ID, ConfigDef.Type.STRING, DEFAULT_PRODUCER_CLIENT_ID, ConfigDef.Importance.LOW, ConfigConstants.PRODUCER_CLIENT_ID_DOC);
     }
 }
