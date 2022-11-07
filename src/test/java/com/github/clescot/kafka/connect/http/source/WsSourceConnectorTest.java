8e9719757cd3ee708486023a2ba7251c39700093
Charles Lescot <910188+clescot@users.noreply.github.com>
2022-11-07 19:51:25 +0100

    add tests for queue name parameters

--- src/test/java/com/github/clescot/kafka/connect/http/source/WsSourceConnectorTest.java
+++ src/test/java/com/github/clescot/kafka/connect/http/source/WsSourceConnectorTest.java
@@ -10,6 +10,8 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Map;
 
+import static com.github.clescot.kafka.connect.http.QueueFactory.DEFAULT_QUEUE_NAME;
+import static com.github.clescot.kafka.connect.http.sink.config.ConfigConstants.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class WsSourceConnectorTest {
@@ -25,9 +27,48 @@ class WsSourceConnectorTest {
     @Test
     public void test_start_nominal_case(){
         Map<String,String> settings = Maps.newHashMap();
-        settings.put("ack.topic","foo");
+        settings.put(SUCCESS_TOPIC,"foo");
+        settings.put(ERRORS_TOPIC,"foo");
         wsSourceConnector.start(settings);
     }
+
+    @Test
+    public void test_start_missing_success_topic(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->  {
+                Map < String, String > settings = Maps.newHashMap();
+                settings.put(ERRORS_TOPIC, "foo");
+                wsSourceConnector.start(settings);
+        });
+    }
+
+    @Test
+    public void test_start_missing_errors_topic(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->  {
+                Map < String, String > settings = Maps.newHashMap();
+                settings.put(SUCCESS_TOPIC, "foo");
+                wsSourceConnector.start(settings);
+        });
+    }
+
+    @Test
+    public void test_start_with_queue_name(){
+            Map < String, String > settings = Maps.newHashMap();
+            settings.put(SUCCESS_TOPIC, "foo1");
+            settings.put(ERRORS_TOPIC, "foo2");
+            settings.put(QUEUE_NAME, "myQueue");
+            wsSourceConnector.start(settings);
+    }
+
+    @Test
+    public void test_start_with_default_queue_name(){
+        Map < String, String > settings = Maps.newHashMap();
+        settings.put(SUCCESS_TOPIC, "foo1");
+        settings.put(ERRORS_TOPIC, "foo2");
+        settings.put(QUEUE_NAME, DEFAULT_QUEUE_NAME);
+        wsSourceConnector.start(settings);
+    }
+
+
 
     @Test
     public void test_start_empty_settings_map(){
@@ -43,7 +84,8 @@ class WsSourceConnectorTest {
     @Test
     public void test_task_configs_zero_task(){
         Map<String,String> settings = Maps.newHashMap();
-        settings.put("ack.topic","foo");
+        settings.put(SUCCESS_TOPIC,"foo");
+        settings.put(ERRORS_TOPIC,"foo");
         wsSourceConnector.start(settings);
         List<Map<String, String>> maps = wsSourceConnector.taskConfigs(0);
         assertThat(maps).asList().isEmpty();
@@ -52,7 +94,8 @@ class WsSourceConnectorTest {
     @Test
     public void test_task_configs_1_task(){
         Map<String,String> settings = Maps.newHashMap();
-        settings.put("ack.topic","foo");
+        settings.put(SUCCESS_TOPIC,"foo");
+        settings.put(ERRORS_TOPIC,"foo");
         wsSourceConnector.start(settings);
         List<Map<String, String>> maps = wsSourceConnector.taskConfigs(1);
         assertThat(maps).asList().hasSize(1);
@@ -61,7 +104,8 @@ class WsSourceConnectorTest {
       @Test
     public void test_task_configs_10_tasks(){
         Map<String,String> settings = Maps.newHashMap();
-        settings.put("ack.topic","foo");
+          settings.put(SUCCESS_TOPIC,"foo");
+          settings.put(ERRORS_TOPIC,"foo");
         wsSourceConnector.start(settings);
           List<Map<String, String>> maps = wsSourceConnector.taskConfigs(10);
           assertThat(maps).asList().hasSize(10);
