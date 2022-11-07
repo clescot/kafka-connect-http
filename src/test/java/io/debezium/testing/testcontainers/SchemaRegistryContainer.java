b29fe9e9216c26daa6975671dffdd33c3620241b
Charles Lescot <910188+clescot@users.noreply.github.com>
2022-11-07 19:51:23 +0100

    upgrade schema registry to 7.2.2

--- src/test/java/io/debezium/testing/testcontainers/SchemaRegistryContainer.java
+++ src/test/java/io/debezium/testing/testcontainers/SchemaRegistryContainer.java
@@ -12,12 +12,15 @@ import org.testcontainers.utility.DockerImageName;
 
 public class SchemaRegistryContainer extends GenericContainer<SchemaRegistryContainer> {
 
-    private static final String SCHEMA_REGISTRY_DOCKER_IMAGE_NAME = "confluentinc/cp-schema-registry:6.0.2";
-    private static final DockerImageName SCHEMA_REGISTRY_DOCKER_IMAGE = DockerImageName.parse(SCHEMA_REGISTRY_DOCKER_IMAGE_NAME);
+    private static final String SCHEMA_REGISTRY_DOCKER_IMAGE_NAME = "confluentinc/cp-schema-registry:7.2.2";
     private static final Integer SCHEMA_REGISTRY_EXPOSED_PORT = 8081;
 
     public SchemaRegistryContainer() {
-        super(SCHEMA_REGISTRY_DOCKER_IMAGE);
+        this(SCHEMA_REGISTRY_DOCKER_IMAGE_NAME);
+    }
+
+    public SchemaRegistryContainer(String dockerImage) {
+        super(dockerImage);
         addExposedPorts(SCHEMA_REGISTRY_EXPOSED_PORT);
     }
 
