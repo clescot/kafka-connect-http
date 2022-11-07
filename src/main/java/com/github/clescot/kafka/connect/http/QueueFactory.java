93290475ea59b50f12dab3c6309b504122045b57
Charles Lescot <910188+clescot@users.noreply.github.com>
2022-11-07 19:51:23 +0100

    add registerConsumerForQueue and hasAConsumer methods for QueueFactory

--- src/main/java/com/github/clescot/kafka/connect/http/QueueFactory.java
+++ src/main/java/com/github/clescot/kafka/connect/http/QueueFactory.java
@@ -1,6 +1,7 @@
 package com.github.clescot.kafka.connect.http;
 
 import com.github.clescot.kafka.connect.http.source.Acknowledgement;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,8 @@ public class QueueFactory {
     public static final String DEFAULT_QUEUE_NAME = "default";
     private static Logger LOGGER = LoggerFactory.getLogger(QueueFactory.class);
     private static Map<String,Queue<Acknowledgement>> queueMap = Maps.newHashMap();
+
+    private static Map<String,Boolean> consumers = Maps.newHashMap();
     public static synchronized Queue<Acknowledgement> getQueue(String queueName){
         if(queueMap.get(queueName) == null){
             LOGGER.debug("creating the '{}' queue",queueName);
@@ -26,5 +29,16 @@ public class QueueFactory {
 
     public static boolean queueMapIsEmpty(){
         return queueMap.isEmpty();
+    }
+
+    public static void registerConsumerForQueue(String queueName){
+        Preconditions.checkNotNull(queueName,"we cannot register a consumer for a null queueName");
+        Preconditions.checkArgument(!queueName.isEmpty(),"we cannot register a consumer for an empty queueName");
+        consumers.put(queueName,true);
+    }
+
+    public static boolean hasAConsumer(String queueName){
+        Boolean queueHasAConsumer = consumers.get(queueName);
+        return queueHasAConsumer != null && queueHasAConsumer;
     }
 }
