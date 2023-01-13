3fd96777f8ac09ad02422d38e811619d4ff9791d
Charles Lescot <910188+clescot@users.noreply.github.com>
2023-01-13 02:11:05 +0100

    add to HttpClientFactory methods getSSLSocketFactory and getKeyManagerFactory

--- src/main/java/com/github/clescot/kafka/connect/http/sink/client/HttpClientFactory.java
+++ src/main/java/com/github/clescot/kafka/connect/http/sink/client/HttpClientFactory.java
@@ -1,14 +1,16 @@
 package com.github.clescot.kafka.connect.http.sink.client;
 
+import javax.annotation.Nullable;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 import java.security.cert.CertificateException;
 import java.util.Map;
 import java.util.Optional;
@@ -19,7 +21,10 @@ public interface HttpClientFactory {
 
 
 
-    static TrustManagerFactory getTrustManagerFactory(String trustStorePath, char[] password, String keystoreType, String algorithm) {
+    static TrustManagerFactory getTrustManagerFactory(String trustStorePath,
+                                                      char[] password,
+                                                      @Nullable String keystoreType,
+                                                      @Nullable String algorithm) {
         TrustManagerFactory trustManagerFactory;
         KeyStore trustStore;
         try {
@@ -40,6 +45,46 @@ public interface HttpClientFactory {
             throw new RuntimeException(e);
         }
         return trustManagerFactory;
+    }
+    static KeyManagerFactory getKeyManagerFactory(String keyStorePath,
+                                                  char[] password,
+                                                  @Nullable String keystoreType,
+                                                  @Nullable String algorithm) {
+        KeyManagerFactory keyManagerFactory;
+        KeyStore keyStore;
+        try {
+            String finalAlgorithm = Optional.ofNullable(algorithm).orElse(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory = KeyManagerFactory.getInstance(finalAlgorithm);
+            String finalKeystoreType = Optional.ofNullable(keystoreType).orElse(KeyStore.getDefaultType());
+            keyStore = KeyStore.getInstance(finalKeystoreType);
+        } catch (NoSuchAlgorithmException | KeyStoreException e) {
+            throw new RuntimeException(e);
+        }
+
+        Path path = Path.of(keyStorePath);
+        File file = path.toFile();
+        try (InputStream inputStream = new FileInputStream(file)) {
+            keyStore.load(inputStream, password);
+            keyManagerFactory.init(keyStore,password);
+        } catch (IOException | NoSuchAlgorithmException | CertificateException | KeyStoreException |
+                 UnrecoverableKeyException e) {
+            throw new RuntimeException(e);
+        }
+        return keyManagerFactory;
+    }
+
+
+    static SSLSocketFactory getSSLSocketFactory(KeyManagerFactory keyManagerFactory,
+                                                TrustManagerFactory trustManagerFactory,
+                                                @Nullable String protocol){
+        try {
+            SSLContext sslContext = SSLContext.getInstance(Optional.ofNullable(protocol).orElse("SSL"));
+            SecureRandom random = new SecureRandom();
+            sslContext.init(keyManagerFactory.getKeyManagers(),trustManagerFactory.getTrustManagers(), random);
+            return sslContext.getSocketFactory();
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     HttpClient build(Map<String, String> config);
