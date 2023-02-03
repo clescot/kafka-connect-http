1b164effcebfd639b096e26b62ee2e3cb6c343ba
Charles Lescot <910188+clescot@users.noreply.github.com>
2023-02-03 10:46:43 +0100

    reorganize modules #1

---
+++ kafka-connect-http-core/src/main/java/io/github/clescot/kafka/connect/http/core/HttpExchange.java
@@ -0,0 +1,197 @@
+package io.github.clescot.kafka.connect.http.core;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@io.confluent.kafka.schemaregistry.annotations.Schema(value = HttpExchange.SCHEMA_AS_STRING,refs = {})
+public class HttpExchange implements Serializable {
+
+    public static final long serialVersionUID = 1L;
+    public static final String SCHEMA_AS_STRING = "{\n" +
+            "  \"$id\": \"https://github.com/clescot/kafka-connect-http-sink/schemas/http-exchange.json\",\n" +
+            "  \"$schema\": \"http://json-schema.org/draft/2019-09/schema\",\n" +
+            "  \"title\": \"Http Exchange\",\n" +
+            "  \"type\": \"object\",\n" +
+            "  \"javaType\" : \"com.github.clescot.kafka.connect.http.HttpExchange\",\n" +
+            "  \"additionalProperties\": false,\n" +
+            "  \"properties\": {\n" +
+            "    \"durationInMillis\": {\n" +
+            "      \"type\": \"integer\"\n" +
+            "    },\n" +
+            "    \"moment\": {\n" +
+            "      \"type\": \"number\"\n" +
+            "    },\n" +
+            "    \"attempts\": {\n" +
+            "      \"type\": \"integer\"\n" +
+            "    },\n" +
+            "    \"success\": {\n" +
+            "      \"type\": \"boolean\"\n" +
+            "    },\n" +
+            "    \"httpResponse\": {\n" +
+            "      \"$ref\": \"https://github.com/clescot/kafka-connect-http-sink/schemas/http-response.json\"\n" +
+            "    },\n" +
+            "    \"httpRequest\": {\n" +
+            "      \"$ref\": \"https://github.com/clescot/kafka-connect-http-sink/schemas/http-request.json\"\n" +
+            "    }\n" +
+            "  },\n" +
+            "  \"required\": [\n" +
+            "    \"moment\",\n" +
+            "    \"attempts\",\n" +
+            "    \"success\",\n" +
+            "    \"httpRequest\",\n" +
+            "    \"httpResponse\"\n" +
+            "  ]\n" +
+            "\n" +
+            "}";
+
+    private Long durationInMillis;
+    private OffsetDateTime moment;
+    private AtomicInteger attempts;
+    private boolean success;
+    private HttpResponse httpResponse;
+    private HttpRequest httpRequest;
+
+    protected HttpExchange() {
+    }
+
+    public HttpExchange(
+            HttpRequest httpRequest,
+            HttpResponse httpResponse,
+            long durationInMillis,
+            OffsetDateTime moment,
+            AtomicInteger attempts,
+            boolean success) {
+        this.httpRequest = httpRequest;
+        this.httpResponse = httpResponse;
+        this.durationInMillis = durationInMillis;
+        this.moment = moment;
+        this.attempts = attempts;
+        this.success = success;
+    }
+
+
+    public OffsetDateTime getMoment() {
+        return moment;
+    }
+
+    public AtomicInteger getAttempts() {
+        return attempts;
+    }
+
+    public Long getDurationInMillis() {
+        return durationInMillis;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public HttpRequest getHttpRequest() {
+        return httpRequest;
+    }
+
+    public HttpResponse getHttpResponse() {
+        return httpResponse;
+    }
+
+    protected void setDurationInMillis(Long durationInMillis) {
+        this.durationInMillis = durationInMillis;
+    }
+
+    protected void setMoment(OffsetDateTime moment) {
+        this.moment = moment;
+    }
+
+    protected void setAttempts(AtomicInteger attempts) {
+        this.attempts = attempts;
+    }
+
+    protected void setHttpResponse(HttpResponse httpResponse) {
+        this.httpResponse = httpResponse;
+    }
+
+    protected void setHttpRequest(HttpRequest httpRequest) {
+        this.httpRequest = httpRequest;
+    }
+
+    @Override
+    public String toString() {
+        return "HttpExchange{" +
+                "durationInMillis=" + durationInMillis +
+                ", moment=" + moment +
+                ", attempts=" + attempts +
+                ", success=" + success +
+                ", httpRequest=" + httpRequest +
+                ", httpResponse=" + httpResponse +
+                '}';
+    }
+
+
+    public static final class Builder {
+        private Long durationInMillis;
+        private OffsetDateTime moment;
+        private AtomicInteger attempts;
+
+        private boolean success;
+        private HttpRequest httpRequest;
+        private HttpResponse httpResponse;
+
+        private Builder() {
+        }
+
+        public static Builder anHttpExchange() {
+            return new Builder();
+        }
+
+
+        public Builder withHttpRequest(HttpRequest httpRequest) {
+            this.httpRequest = httpRequest;
+            return this;
+        }
+
+        public Builder withHttpResponse(HttpResponse httpResponse) {
+            this.httpResponse = httpResponse;
+            return this;
+        }
+
+
+        public Builder withSuccess(boolean success) {
+            this.success = success;
+            return this;
+        }
+
+
+        public Builder withDuration(Long durationInMillis) {
+            this.durationInMillis = durationInMillis;
+            return this;
+        }
+
+        public Builder at(OffsetDateTime moment) {
+            this.moment = moment;
+            return this;
+        }
+
+        public Builder withAttempts(AtomicInteger attempts) {
+            this.attempts = attempts;
+            return this;
+        }
+
+        public HttpExchange build() {
+            return new HttpExchange(
+                    httpRequest,
+                    httpResponse,
+                    durationInMillis,
+                    moment,
+                    attempts,
+                    success
+            );
+        }
+    }
+
+
+}
---
+++ kafka-connect-http-core/src/main/java/io/github/clescot/kafka/connect/http/core/HttpResponseAsStruct.java
@@ -0,0 +1,41 @@
+package io.github.clescot.kafka.connect.http.core;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+
+public class HttpResponseAsStruct {
+    private static final Integer VERSION = 1;
+
+    private static final String STATUS_CODE = "statusCode";
+    private static final String STATUS_MESSAGE = "statusMessage";
+    private static final String PROTOCOL = "protocol";
+    private static final String RESPONSE_HEADERS = "responseHeaders";
+    private static final String RESPONSE_BODY = "responseBody";
+
+    public static final Schema SCHEMA = SchemaBuilder
+            .struct()
+            .name(HttpResponse.class.getName())
+            .version(VERSION)
+            .field(STATUS_CODE,Schema.INT64_SCHEMA)
+            .field(STATUS_MESSAGE,Schema.STRING_SCHEMA)
+            .field(PROTOCOL,Schema.OPTIONAL_STRING_SCHEMA)
+            .field(RESPONSE_HEADERS, SchemaBuilder.map(Schema.STRING_SCHEMA, SchemaBuilder.array(Schema.STRING_SCHEMA)).build())
+            .field(RESPONSE_BODY,Schema.OPTIONAL_STRING_SCHEMA);
+
+    private HttpResponse httpResponse;
+
+    public HttpResponseAsStruct(HttpResponse httpResponse) {
+        this.httpResponse = httpResponse;
+    }
+
+    public Struct toStruct() {
+        return new Struct(SCHEMA)
+                .put(STATUS_CODE,httpResponse.getStatusCode().longValue())
+                .put(STATUS_MESSAGE,httpResponse.getStatusMessage())
+                .put(RESPONSE_HEADERS,httpResponse.getResponseHeaders())
+                .put(RESPONSE_BODY,httpResponse.getResponseBody());
+
+
+    }
+}
