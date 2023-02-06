b4345bfd9cc8068fd996a4a22c56856a3d725e8d
charles lescot <910188+clescot@users.noreply.github.com>
2023-02-07 00:27:35 +0100

    - add moreserializations tests. (#168)
    - fix HttpResponse serialization issue when protocol is not set (not required field)

--- kafka-connect-http-connectors/src/test/java/io/github/clescot/kafka/connect/http/sink/model/HttpExchangeTest.java
+++ kafka-connect-http-connectors/src/test/java/io/github/clescot/kafka/connect/http/sink/model/HttpExchangeTest.java
@@ -1,6 +1,12 @@
 package io.github.clescot.kafka.connect.http.sink.model;
 
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.json.JsonSchemaProvider;
+import io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializer;
+import io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializerConfig;
 import io.github.clescot.kafka.connect.http.core.HttpExchange;
 import io.github.clescot.kafka.connect.http.core.HttpRequest;
 import io.github.clescot.kafka.connect.http.core.HttpResponse;
@@ -11,6 +17,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.time.OffsetDateTime;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.github.clescot.kafka.connect.http.sink.client.ahc.AHCHttpClient.SUCCESS;
@@ -89,6 +96,35 @@ public class HttpExchangeTest {
                     null
             );
             System.out.println(expectedJsonSchema);
+        }
+        @Test
+        public void test_serialize_http_exchange(){
+            int statusCode = 200;
+            HttpExchange httpExchange = new HttpExchange(
+                    getDummyHttpRequest(),
+                    getDummyHttpResponse(statusCode),
+                    745L,
+                    OffsetDateTime.now(),
+                    new AtomicInteger(2),
+                    SUCCESS
+            );
+            SpecificationVersion jsonSchemaSpecification = SpecificationVersion.DRAFT_2019_09;
+            boolean useOneOfForNullables=false;
+            boolean failUnknownProperties=true;
+            Map<String,String> jsonSchemaSerializerConfig = Maps.newHashMap();
+            jsonSchemaSerializerConfig.put(KafkaJsonSchemaSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG,"mock://stuff.com");
+            jsonSchemaSerializerConfig.put(KafkaJsonSchemaSerializerConfig.SCHEMA_SPEC_VERSION,jsonSchemaSpecification.toString());
+            jsonSchemaSerializerConfig.put(KafkaJsonSchemaSerializerConfig.WRITE_DATES_AS_ISO8601,"true");
+            jsonSchemaSerializerConfig.put(KafkaJsonSchemaSerializerConfig.ONEOF_FOR_NULLABLES,""+useOneOfForNullables);
+            jsonSchemaSerializerConfig.put(KafkaJsonSchemaSerializerConfig.FAIL_UNKNOWN_PROPERTIES,""+failUnknownProperties);
+            jsonSchemaSerializerConfig.put(KafkaJsonSchemaSerializerConfig.FAIL_INVALID_SCHEMA,""+true);
+
+            MockSchemaRegistryClient schemaRegistryClient = new MockSchemaRegistryClient(Lists.newArrayList(new JsonSchemaProvider()));
+
+            KafkaJsonSchemaSerializer<HttpExchange> serializer = new KafkaJsonSchemaSerializer<>(schemaRegistryClient,jsonSchemaSerializerConfig);
+            byte[] bytes = serializer.serialize("dummy_topic", httpExchange);
+            assertThat(bytes).isNotEmpty();
+
         }
 }
 
