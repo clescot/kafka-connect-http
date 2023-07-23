75c2e9cc14a3961fccce127007b73e4398780289
Charles Lescot <910188+clescot@users.noreply.github.com>
2023-07-23 19:43:20 +0200

    add WeightedProxySelector

---
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/sink/client/proxy/WeightedProxySelector.java
@@ -0,0 +1,33 @@
+package io.github.clescot.kafka.connect.http.sink.client.proxy;
+
+import java.io.IOException;
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.SocketAddress;
+import java.net.URI;
+import java.util.List;
+import java.util.NavigableMap;
+import java.util.Random;
+
+public class WeightedProxySelector extends ProxySelector {
+
+    private final NavigableMap<Integer, Proxy> proxies;
+    private final Random random;
+    private final int totalWeight;
+
+    public WeightedProxySelector(NavigableMap<Integer,Proxy> proxies, Random random) {
+        this.proxies = proxies;
+        this.random = random;
+        this.totalWeight = proxies.keySet().stream().reduce(0, Integer::sum);
+    }
+
+    @Override
+    public List<Proxy> select(URI uri) {
+        return List.of(proxies.ceilingEntry(random.nextInt(totalWeight)).getValue());
+    }
+
+    @Override
+    public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
+
+    }
+}
