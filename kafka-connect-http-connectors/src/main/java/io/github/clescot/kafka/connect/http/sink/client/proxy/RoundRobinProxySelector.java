4e090cead644c742baa9f0f915fadc7d8943161b
Charles Lescot <910188+clescot@users.noreply.github.com>
2023-07-23 23:00:10 +0200

    add RoundRobinProxySelector

---
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/sink/client/proxy/RoundRobinProxySelector.java
@@ -0,0 +1,33 @@
+package io.github.clescot.kafka.connect.http.sink.client.proxy;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Iterables;
+
+import java.io.IOException;
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.SocketAddress;
+import java.net.URI;
+import java.util.Iterator;
+import java.util.List;
+
+public class RoundRobinProxySelector extends ProxySelector {
+
+    private final Iterator<Proxy> proxies;
+
+    public RoundRobinProxySelector(List<Proxy> proxies) {
+        Preconditions.checkNotNull(proxies,"proxies list must not be null");
+        Preconditions.checkArgument(!proxies.isEmpty(),"proxies list must not be empty");
+        this.proxies = Iterables.cycle(proxies).iterator();
+    }
+
+    @Override
+    public List<Proxy> select(URI uri) {
+        return List.of(proxies.next());
+    }
+
+    @Override
+    public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
+
+    }
+}
