e783e7b2b340e87edf582b49ab7a3a6ea520df17
Charles Lescot <910188+clescot@users.noreply.github.com>
2023-08-04 15:44:04 +0200

    comment and disable sonar issue.

--- kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/sink/client/ssl/AlwaysTrustManager.java
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/sink/client/ssl/AlwaysTrustManager.java
@@ -4,15 +4,21 @@ import javax.net.ssl.X509TrustManager;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
+/**
+ * Use of this class cause is security issue. Use it at your own risk !
+ * It should be used only for tests.
+ */
 public class AlwaysTrustManager implements X509TrustManager {
+    @SuppressWarnings("java:S4830")
     @Override
     public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-
+        // method is empty to accept all client certificates
     }
 
+    @SuppressWarnings("java:S4830")
     @Override
     public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-
+            // method is empty to accept all server certificates
     }
 
     @Override
