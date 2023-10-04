9ea88bf3b7fa174ce4ddba2b84c61861f317b3f2
Charles Lescot <910188+clescot@users.noreply.github.com>
2023-10-05 01:42:48 +0200

    fix tests.

--- kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/client/okhttp/SSLHandshakeInterceptor.java
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/client/okhttp/SSLHandshakeInterceptor.java
@@ -18,32 +18,34 @@ public class SSLHandshakeInterceptor implements Interceptor {
     public Response intercept(@NotNull Chain chain) throws IOException {
         Request request = chain.request();
         Handshake handshake = chain.connection().handshake();
-
-        //cipher
-        CipherSuite cipherSuite = handshake.cipherSuite();
-        String cipherSuiteJavaName = cipherSuite.javaName();
-        LOGGER.debug("cipherSuiteJavaName:{}",cipherSuiteJavaName);
-        //local
-        Principal localPrincipal = handshake.localPrincipal();
-        if(localPrincipal!=null) {
-            String localPrincipalName = localPrincipal.getName();
-            LOGGER.debug("local principal:{}", localPrincipalName);
+        if(handshake!=null) {
+            //cipher
+            CipherSuite cipherSuite = handshake.cipherSuite();
+            if (cipherSuite != null) {
+                String cipherSuiteJavaName = cipherSuite.javaName();
+                LOGGER.debug("cipherSuiteJavaName:{}", cipherSuiteJavaName);
+            }
+            //local
+            Principal localPrincipal = handshake.localPrincipal();
+            if (localPrincipal != null) {
+                String localPrincipalName = localPrincipal.getName();
+                LOGGER.debug("local principal:{}", localPrincipalName);
+            }
+            List<Certificate> localCertificates = handshake.localCertificates();
+            for (Certificate localCertificate : localCertificates) {
+                LOGGER.debug("local certificate:{}", localCertificate);
+            }
+            //remote
+            Principal peerPrincipal = handshake.peerPrincipal();
+            if (peerPrincipal != null) {
+                String peerPrincipalName = peerPrincipal.getName();
+                LOGGER.debug("PEER principal:{}", peerPrincipalName);
+            }
+            List<Certificate> peerCertificates = handshake.peerCertificates();
+            for (Certificate peerCertificate : peerCertificates) {
+                LOGGER.debug("peer certificate:{}", peerCertificate);
+            }
         }
-        List<Certificate> localCertificates = handshake.localCertificates();
-        for (Certificate localCertificate : localCertificates) {
-            LOGGER.debug("local certificate:{}",localCertificate);
-        }
-        //remote
-        Principal peerPrincipal = handshake.peerPrincipal();
-        if(peerPrincipal!=null) {
-            String peerPrincipalName = peerPrincipal.getName();
-            LOGGER.debug("PEER principal:{}", peerPrincipalName);
-        }
-        List<Certificate> peerCertificates = handshake.peerCertificates();
-        for (Certificate peerCertificate : peerCertificates) {
-            LOGGER.debug("peer certificate:{}",peerCertificate);
-        }
-
 
         Response response = chain.proceed(request);
         return response;
