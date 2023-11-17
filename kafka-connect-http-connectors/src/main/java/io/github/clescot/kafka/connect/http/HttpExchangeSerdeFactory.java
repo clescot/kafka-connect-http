3353bb6aaef48b3e5c394ea61649a00710f5f953
Charles Lescot <910188+clescot@users.noreply.github.com>
2023-11-17 20:20:20 +0100

    add logs

--- kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/HttpExchangeSerdeFactory.java
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/HttpExchangeSerdeFactory.java
@@ -1,27 +1,30 @@
 package io.github.clescot.kafka.connect.http;
 
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.streams.serdes.json.KafkaJsonSchemaSerde;
 import io.github.clescot.kafka.connect.http.core.HttpExchange;
 import org.apache.kafka.common.serialization.Serde;
-import io.confluent.kafka.streams.serdes.json.KafkaJsonSchemaSerde;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Map;
 
 public class HttpExchangeSerdeFactory {
 
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpExchangeSerdeFactory.class);
     private final SchemaRegistryClient schemaRegistryClient;
+    private final Map<String, Object> serdeConfig;
 
-    private final JsonSchemaSerdeConfigFactory jsonSchemaSerdeFactory;
 
     public HttpExchangeSerdeFactory(SchemaRegistryClient schemaRegistryClient,
-                                    JsonSchemaSerdeConfigFactory jsonSchemaSerdeFactory) {
+                                    Map<String, Object> serdeConfig) {
         this.schemaRegistryClient = schemaRegistryClient;
-        this.jsonSchemaSerdeFactory = jsonSchemaSerdeFactory;
+        this.serdeConfig = serdeConfig;
     }
 
     public Serde<HttpExchange> buildValueSerde(){
         final KafkaJsonSchemaSerde<HttpExchange> jsonSchemaSerde = new KafkaJsonSchemaSerde<>(schemaRegistryClient,HttpExchange.class);
-        Map<String, Object> serdeConfig = jsonSchemaSerdeFactory.getConfig();
+        serdeConfig.entrySet().forEach(entry-> LOGGER.info("{}:{}",entry.getKey(),entry.getValue()));
         jsonSchemaSerde.configure(serdeConfig, false);
         return jsonSchemaSerde;
     }
