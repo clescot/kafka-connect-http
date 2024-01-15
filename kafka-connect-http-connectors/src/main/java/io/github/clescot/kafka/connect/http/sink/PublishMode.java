25f6314cfad6c6c4b2cebfa7072863dd4af1e51e
Charles Lescot <910188+clescot@users.noreply.github.com>
2024-01-16 00:18:21 +0100

    add javadoc comments in PublishMode

--- kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/sink/PublishMode.java
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/sink/PublishMode.java
@@ -1,7 +1,18 @@
 package io.github.clescot.kafka.connect.http.sink;
 
 public enum PublishMode {
+    /**
+     *  with a Sink Connector, grab HTTP messages to query from a topic, publish HTTP results in an in memory queue, which will be published
+     *  in a resulting topic with a source Connector
+     */
     IN_MEMORY_QUEUE,
+    /**
+     * with a Sink Connector, grab HTTP messages to query from a topic, and publish HTTP results in a final topic
+     * with an embedded low level producer
+     */
     PRODUCER,
+    /**
+     * with a Sink Connector, grab HTTP messages to query from a topic, and does NOT publish HTTP results
+     */
     NONE
 }
