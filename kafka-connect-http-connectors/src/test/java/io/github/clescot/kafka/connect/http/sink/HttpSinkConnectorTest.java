4a6ca5d0b1b714abb4a5a24cf685ca4d34b86541
Charles Lescot <910188+clescot@users.noreply.github.com>
2024-01-25 18:52:39 +0100

    reorganise tests.

--- kafka-connect-http-connectors/src/test/java/io/github/clescot/kafka/connect/http/sink/HttpSinkConnectorTest.java
+++ kafka-connect-http-connectors/src/test/java/io/github/clescot/kafka/connect/http/sink/HttpSinkConnectorTest.java
@@ -3,6 +3,7 @@ package io.github.clescot.kafka.connect.http.sink;
 import com.google.common.collect.Maps;
 import org.apache.kafka.connect.connector.Task;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -12,37 +13,45 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpSinkConnectorTest {
 
-
-
+    @Nested
+    class TestStart {
         @Test
-        void test_start_with_empty_map(){
+        void test_start_with_empty_map() {
             HttpSinkConnector httpSinkConnector = new HttpSinkConnector();
-            Assertions.assertDoesNotThrow(()->httpSinkConnector.start(Maps.newHashMap()));
-        }
-        @Test
-        void test_start_with_null_map(){
-            HttpSinkConnector httpSinkConnector = new HttpSinkConnector();
-            Assertions.assertThrows(NullPointerException.class,()->httpSinkConnector.start(null));
+            Assertions.assertDoesNotThrow(() -> httpSinkConnector.start(Maps.newHashMap()));
         }
 
         @Test
-        void test_start_with_nominal_case(){
+        void test_start_with_null_map() {
+            HttpSinkConnector httpSinkConnector = new HttpSinkConnector();
+            Assertions.assertThrows(NullPointerException.class, () -> httpSinkConnector.start(null));
+        }
+
+        @Test
+        void test_start_with_nominal_case() {
             HttpSinkConnector httpSinkConnector = new HttpSinkConnector();
             Map<String, String> settings = Maps.newHashMap();
-            settings.put("config.default.httpclient.ssl.truststore.always.trust","true");
-            Assertions.assertDoesNotThrow(()->httpSinkConnector.start(settings));
+            settings.put("config.default.httpclient.ssl.truststore.always.trust", "true");
+            Assertions.assertDoesNotThrow(() -> httpSinkConnector.start(settings));
         }
+    }
 
+    @Nested
+    class TestTaskClass {
 
         @Test
-        void test_task_class_nominal_case(){
+        void test_task_class_nominal_case() {
             HttpSinkConnector httpSinkConnector = new HttpSinkConnector();
             Class<? extends Task> aClass = httpSinkConnector.taskClass();
             assertThat(aClass).isEqualTo(HttpSinkTask.class);
         }
 
+    }
+
+    @Nested
+    class TestTaskConfig {
         @Test
-        void test_taskConfigs_nominal_case(){
+        void test_taskConfigs_nominal_case() {
             HttpSinkConnector httpSinkConnector = new HttpSinkConnector();
             Map<String, String> settings = Maps.newHashMap();
             httpSinkConnector.start(settings);
@@ -53,18 +62,18 @@ public class HttpSinkConnectorTest {
 
 
         @Test
-        void test_calling_task_configs_but_not_start(){
+        void test_calling_task_configs_but_not_start() {
             HttpSinkConnector httpSinkConnector = new HttpSinkConnector();
-            Assertions.assertThrows(NullPointerException.class,()->{
-                    httpSinkConnector.taskConfigs(1);
-                }
+            Assertions.assertThrows(NullPointerException.class, () -> {
+                        httpSinkConnector.taskConfigs(1);
+                    }
             );
 
         }
 
 
         @Test
-        void test_2_tasks(){
+        void test_2_tasks() {
             HttpSinkConnector httpSinkConnector = new HttpSinkConnector();
             Map<String, String> settings = Maps.newHashMap();
             httpSinkConnector.start(settings);
@@ -73,15 +82,19 @@ public class HttpSinkConnectorTest {
             assertThat(maps.get(0)).isEqualTo(settings);
             assertThat(maps.get(1)).isEqualTo(settings);
         }
+    }
 
-
+    @Nested
+    class TestStop {
         @Test
-        void test_stop_nominal_case_without_ack_sender_already_initialized(){
+        void test_stop_nominal_case_without_ack_sender_already_initialized() {
             HttpSinkConnector httpSinkConnector = new HttpSinkConnector();
             Map<String, String> settings = Maps.newHashMap();
             httpSinkConnector.start(settings);
             Assertions.assertDoesNotThrow(httpSinkConnector::stop);
-
         }
+
+    }
+
 
 }
