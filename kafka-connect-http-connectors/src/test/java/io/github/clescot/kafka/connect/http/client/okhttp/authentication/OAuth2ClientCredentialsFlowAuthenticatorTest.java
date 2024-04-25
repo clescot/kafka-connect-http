2214998cbd3993e5fbbbd537ee1eea4736da6674
Charles Lescot <910188+clescot@users.noreply.github.com>
2024-04-26 00:17:48 +0200

    add tests

--- kafka-connect-http-connectors/src/test/java/io/github/clescot/kafka/connect/http/client/okhttp/authentication/OAuth2ClientCredentialsFlowAuthenticatorTest.java
+++ kafka-connect-http-connectors/src/test/java/io/github/clescot/kafka/connect/http/client/okhttp/authentication/OAuth2ClientCredentialsFlowAuthenticatorTest.java
@@ -6,7 +6,7 @@ import com.github.tomakehurst.wiremock.core.Options;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
-import okhttp3.OkHttpClient;
+import okhttp3.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,12 +17,19 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 class OAuth2ClientCredentialsFlowAuthenticatorTest {
+    public static final String CLIENT_ID = "44d34a4d05344c97837d463207805f8b";
+    public static final String CLIENT_SECRET = "3fc0576720544ac293a3a5304e6c0fa8";
+    public static final String WELL_KNOWN_OPENID_CONFIGURATION = "/.well-known/openid-configuration";
+    public static final String SONG_PATH = "/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V";
     @RegisterExtension
     static WireMockExtension wmHttp;
+
     static {
 
         wmHttp = WireMockExtension.newInstance()
@@ -33,8 +40,12 @@ class OAuth2ClientCredentialsFlowAuthenticatorTest {
                 )
                 .build();
     }
+
     public static final String WELL_KNOWN_OK = "WellKnownOk";
+    public static final String TOKEN_OK = "TokenOk";
+    public static final String SONG_OK = "SongOk";
     private String httpBaseUrl;
+
     @BeforeEach
     void setup() throws IOException {
         String scenario = "test_successful_request_at_first_time";
@@ -43,10 +54,10 @@ class OAuth2ClientCredentialsFlowAuthenticatorTest {
         Path path = Paths.get("src/test/resources/oauth2/wellknownUrlContent.json");
         httpBaseUrl = wmRuntimeInfo.getHttpBaseUrl();
         String content = Files.readString(path);
-        String wellKnownUrlContent = content.replaceAll("baseUrl",httpBaseUrl);
+        String wellKnownUrlContent = content.replaceAll("baseUrl", httpBaseUrl);
 
         wireMock
-                .register(WireMock.get("/.well-known/openid-configuration").inScenario(scenario)
+                .register(WireMock.get(WELL_KNOWN_OPENID_CONFIGURATION).inScenario(scenario)
                         .whenScenarioStateIs(STARTED)
                         .willReturn(WireMock.aResponse()
                                 .withStatus(200)
@@ -55,9 +66,39 @@ class OAuth2ClientCredentialsFlowAuthenticatorTest {
                         ).willSetStateTo(WELL_KNOWN_OK)
                 );
 
+        Path tokenPath = Paths.get("src/test/resources/oauth2/token.json");
+        String tokenContent = Files.readString(tokenPath);
+        wireMock
+                .register(
+                        WireMock.post("/api/token")
+                                .withHeader("Content-Type",containing("application/x-www-form-urlencoded; charset=UTF-8"))
+                                .withHeader("Authorization",containing("NDRkMzRhNGQwNTM0NGM5NzgzN2Q0NjMyMDc4MDVmOGI6M2ZjMDU3NjcyMDU0NGFjMjkzYTNhNTMwNGU2YzBmYTg="))
+                                .inScenario(scenario)
+                                .whenScenarioStateIs(WELL_KNOWN_OK)
+                                .willReturn(WireMock.aResponse()
+                                        .withStatus(200)
+                                        .withStatusMessage("OK")
+                                        .withBody(tokenContent)
+                                ).willSetStateTo(TOKEN_OK)
+                );
 
 
+        Path songPath = Paths.get("src/test/resources/oauth2/song.json");
+        String songContent = Files.readString(songPath);
+        wireMock
+                .register(
+                        WireMock.get(SONG_PATH)
+                                .withHeader("Authorization",containing("Bearer BQDzs98uhifaGayk8H9tCTRozufhFmgV_HKMCnnDdMTdz1FcOo3sdj8OZJ_azo96LRdLI9_1uJOCXxbGZme11KCb6ZxTuCt8B5FxEeECb1kO_-UDuf8"))
+                                .inScenario(scenario)
+                                .whenScenarioStateIs(TOKEN_OK)
+                                .willReturn(WireMock.aResponse()
+                                        .withStatus(200)
+                                        .withStatusMessage("OK")
+                                        .withBody(songContent)
+                                ).willSetStateTo(SONG_OK)
+                );
     }
+
     @Test
     void test_constructor_with_null_args() {
         Assertions.assertThrows(NullPointerException.class, () -> new OAuth2ClientCredentialsFlowAuthenticator(
@@ -67,31 +108,31 @@ class OAuth2ClientCredentialsFlowAuthenticatorTest {
     @Test
     void test_constructor_with_missing_ok_http_client() {
         Assertions.assertThrows(NullPointerException.class, () -> new OAuth2ClientCredentialsFlowAuthenticator(
-                null, httpBaseUrl+"/.well-known/openid-configuration", "1234", "1234?/"));
+                null, httpBaseUrl + WELL_KNOWN_OPENID_CONFIGURATION, CLIENT_ID, CLIENT_SECRET));
     }
 
     @Test
     void test_constructor_with_missing_well_known_url() {
         Assertions.assertThrows(NullPointerException.class, () -> new OAuth2ClientCredentialsFlowAuthenticator(
-                new OkHttpClient(), null, "1234", "1234?/"));
+                new OkHttpClient(), null, CLIENT_ID, CLIENT_SECRET));
     }
 
     @Test
     void test_constructor_with_missing_well_client_id() {
         Assertions.assertThrows(NullPointerException.class, () -> new OAuth2ClientCredentialsFlowAuthenticator(
-                new OkHttpClient(), httpBaseUrl+"/.well-known/openid-configuration", null, "1234?/"));
+                new OkHttpClient(), httpBaseUrl + WELL_KNOWN_OPENID_CONFIGURATION, null, CLIENT_SECRET));
     }
 
     @Test
     void test_constructor_with_missing_well_client_secret() {
         Assertions.assertThrows(NullPointerException.class, () -> new OAuth2ClientCredentialsFlowAuthenticator(
-                new OkHttpClient(), httpBaseUrl+"/.well-known/openid-configuration", "1234", null));
+                new OkHttpClient(), httpBaseUrl + WELL_KNOWN_OPENID_CONFIGURATION, CLIENT_ID, null));
     }
 
     @Test
     void test_constructor_nominal_case() {
         OAuth2ClientCredentialsFlowAuthenticator authenticator = new OAuth2ClientCredentialsFlowAuthenticator(
-                new OkHttpClient(), httpBaseUrl+"/.well-known/openid-configuration", "1234", "1234?/");
+                new OkHttpClient(), httpBaseUrl + WELL_KNOWN_OPENID_CONFIGURATION, CLIENT_ID, CLIENT_SECRET);
         assertThat(authenticator)
                 .isNotNull()
                 .isInstanceOf(OAuth2ClientCredentialsFlowAuthenticator.class);
@@ -100,16 +141,29 @@ class OAuth2ClientCredentialsFlowAuthenticatorTest {
     @Test
     void test_constructor_nominal_case_with_known_scopes() {
         OAuth2ClientCredentialsFlowAuthenticator authenticator = new OAuth2ClientCredentialsFlowAuthenticator(
-                new OkHttpClient(), httpBaseUrl+"/.well-known/openid-configuration", "1234", "1234?/","openid","email");
+                new OkHttpClient(), httpBaseUrl + WELL_KNOWN_OPENID_CONFIGURATION, CLIENT_ID, CLIENT_SECRET, "openid", "email");
         assertThat(authenticator)
                 .isNotNull()
                 .isInstanceOf(OAuth2ClientCredentialsFlowAuthenticator.class);
     }
-  @Test
-    void test_constructor_nominal_case_with_unknown_scopes() {
-      Assertions.assertThrows(IllegalArgumentException.class,()->new OAuth2ClientCredentialsFlowAuthenticator(
-                new OkHttpClient(), httpBaseUrl+"/.well-known/openid-configuration", "1234", "1234?/","opensid","emaissssl"));
-    }
 
+    @Test
+    void test_constructor_nominal_case_with_unknown_scopes() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new OAuth2ClientCredentialsFlowAuthenticator(
+                new OkHttpClient(), httpBaseUrl + WELL_KNOWN_OPENID_CONFIGURATION, CLIENT_ID, CLIENT_SECRET, "opensid", "emaissssl"));
+    }
+    @Test
+    void test_authenticate_nominal_case() throws IOException {
+        Authenticator authenticator = new OAuth2ClientCredentialsFlowAuthenticator(
+                new OkHttpClient(), httpBaseUrl + WELL_KNOWN_OPENID_CONFIGURATION, CLIENT_ID, CLIENT_SECRET);
+        Route route = mock(Route.class);
+        Request request = new Request.Builder().url(httpBaseUrl+SONG_PATH).get().build();
+        Response.Builder builder = new Response.Builder();
+        builder.setRequest$okhttp(request);
+        Response response = builder.code(200).protocol(Protocol.HTTP_1_1).message("OK").build();
+        Request authenticatedRequest = authenticator.authenticate(route, response);
+        String authorizationHeader = authenticatedRequest.headers().get("Authorization");
+        assertThat(authorizationHeader).isEqualTo("Bearer BQDzs98uhifaGayk8H9tCTRozufhFmgV_HKMCnnDdMTdz1FcOo3sdj8OZJ_azo96LRdLI9_1uJOCXxbGZme11KCb6ZxTuCt8B5FxEeECb1kO_-UDuf8");
+    }
 
 }
