10f7fe0aad0f198207552dd1ee1b1cd9d3f4b394
Charles Lescot <910188+clescot@users.noreply.github.com>
2024-06-12 02:24:49 +0200

    add tests.

--- kafka-connect-http-connectors/src/test/java/io/github/clescot/kafka/connect/http/sink/JEXLHttpRequestMapperTest.java
+++ kafka-connect-http-connectors/src/test/java/io/github/clescot/kafka/connect/http/sink/JEXLHttpRequestMapperTest.java
@@ -1,6 +1,7 @@
 package io.github.clescot.kafka.connect.http.sink;
 
 import com.google.common.collect.Lists;
+import io.github.clescot.kafka.connect.http.core.HttpRequest;
 import org.apache.kafka.common.record.TimestampType;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.header.Header;
@@ -16,6 +17,10 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class JEXLHttpRequestMapperTest {
 
+    private static final String DUMMY_BODY = "stuff";
+    private static final String DUMMY_URL = "http://www." + DUMMY_BODY + ".com";
+    private static final String DUMMY_METHOD = "POST";
+    private static final String DUMMY_BODY_TYPE = "STRING";
     @Nested
     class Constructor {
 
@@ -30,19 +35,17 @@ class JEXLHttpRequestMapperTest {
         }
 
         @Test
-        void test_valid_expression() {
+        void test_valid_expression_with_url_as_constant() {
             Assertions.assertDoesNotThrow(()->new JEXLHttpRequestMapper("sinkRecord.topic()=='myTopic'","'http://url.com'",null,null));
         }
+
     }
 
 
     @Nested
     class Matches {
 
-        private static final String DUMMY_BODY = "stuff";
-        private static final String DUMMY_URL = "http://www." + DUMMY_BODY + ".com";
-        private static final String DUMMY_METHOD = "POST";
-        private static final String DUMMY_BODY_TYPE = "STRING";
+
         private SinkRecord sinkRecord;
         @BeforeEach
         public void setup() {
@@ -75,18 +78,66 @@ class JEXLHttpRequestMapperTest {
             assertThat(matches).isFalse();
         }
 
-        private String getDummyHttpRequestAsString() {
-            return "{\n" +
-                    "  \"url\": \"" + DUMMY_URL + "\",\n" +
-                    "  \"headers\": {},\n" +
-                    "  \"method\": \"" + DUMMY_METHOD + "\",\n" +
-                    "  \"bodyAsString\": \"" + DUMMY_BODY + "\",\n" +
-                    "  \"bodyAsByteArray\": [],\n" +
-                    "  \"bodyAsForm\": {},\n" +
-                    "  \"bodyAsMultipart\": [],\n" +
-                    "  \"bodyType\": \"" + DUMMY_BODY_TYPE + "\"\n" +
-                    "}";
-        }
+
     }
 
+    @Nested
+    class Map {
+
+        private static final String DUMMY_BODY = "stuff";
+        private static final String DUMMY_URL = "http://www." + DUMMY_BODY + ".com";
+        private static final String DUMMY_METHOD = "POST";
+        private static final String DUMMY_BODY_TYPE = "STRING";
+        private SinkRecord sinkRecord;
+        @BeforeEach
+        public void setup() {
+            List<Header> headers = Lists.newArrayList();
+            sinkRecord = new SinkRecord("myTopic",
+                    0,
+                    Schema.STRING_SCHEMA,
+                    "key",
+                    Schema.STRING_SCHEMA,
+                    "http://test.com",
+                    -1,
+                    System.currentTimeMillis(),
+                    TimestampType.CREATE_TIME,
+                    headers
+            );
+
+        }
+        @Test
+        void test_url_as_constant(){
+            JEXLHttpRequestMapper httpRequestMapper = new JEXLHttpRequestMapper("sinkRecord.topic()=='myTopic'","'http://url.com'",null,null);
+            boolean matches = httpRequestMapper.matches(sinkRecord);
+            assertThat(matches).isTrue();
+            HttpRequest httpRequest = httpRequestMapper.map(sinkRecord);
+            assertThat(httpRequest.getUrl()).isEqualTo("http://url.com");
+            assertThat(httpRequest.getMethod()).isEqualTo("GET");
+            assertThat(httpRequest.getBodyType()).isEqualTo(HttpRequest.BodyType.STRING);
+        }
+        @Test
+        void test_url_as_variable(){
+            JEXLHttpRequestMapper httpRequestMapper = new JEXLHttpRequestMapper("sinkRecord.topic()=='myTopic'","sinkRecord.value()",null,null);
+            boolean matches = httpRequestMapper.matches(sinkRecord);
+            assertThat(matches).isTrue();
+            HttpRequest httpRequest = httpRequestMapper.map(sinkRecord);
+            assertThat(httpRequest.getUrl()).isEqualTo("http://test.com");
+            assertThat(httpRequest.getMethod()).isEqualTo("GET");
+            assertThat(httpRequest.getBodyType()).isEqualTo(HttpRequest.BodyType.STRING);
+        }
+
+    }
+
+    private String getDummyHttpRequestAsString() {
+        return "{\n" +
+                "  \"url\": \"" + DUMMY_URL + "\",\n" +
+                "  \"headers\": {},\n" +
+                "  \"method\": \"" + DUMMY_METHOD + "\",\n" +
+                "  \"bodyAsString\": \"" + DUMMY_BODY + "\",\n" +
+                "  \"bodyAsByteArray\": [],\n" +
+                "  \"bodyAsForm\": {},\n" +
+                "  \"bodyAsMultipart\": [],\n" +
+                "  \"bodyType\": \"" + DUMMY_BODY_TYPE + "\"\n" +
+                "}";
+    }
 }
