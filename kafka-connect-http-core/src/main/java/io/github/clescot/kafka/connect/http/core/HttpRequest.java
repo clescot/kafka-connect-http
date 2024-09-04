4a7dd38f36827803f45c3b0e5d80c7fe3b103e3b
Charles Lescot <910188+clescot@users.noreply.github.com>
2024-09-05 01:55:37 +0200

    fix sonar

--- kafka-connect-http-core/src/main/java/io/github/clescot/kafka/connect/http/core/HttpRequest.java
+++ kafka-connect-http-core/src/main/java/io/github/clescot/kafka/connect/http/core/HttpRequest.java
@@ -14,7 +14,7 @@ import java.util.*;
         refs = {})
 public class HttpRequest implements Serializable {
 
-    public static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpRequest.class);
 
