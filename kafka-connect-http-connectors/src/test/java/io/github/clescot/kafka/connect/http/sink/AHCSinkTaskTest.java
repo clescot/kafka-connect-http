0d725d5f0f4282ac7aafc0cefc2eb30431f3d863
Charles Lescot <910188+clescot@users.noreply.github.com>
2024-11-01 02:54:12 +0100

    add tests.

---
+++ kafka-connect-http-connectors/src/test/java/io/github/clescot/kafka/connect/http/sink/AHCSinkTaskTest.java
@@ -0,0 +1,20 @@
+package io.github.clescot.kafka.connect.http.sink;
+
+import org.apache.kafka.clients.producer.MockProducer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class AHCSinkTaskTest {
+
+    @Test
+    void test_constructor_with_no_args(){
+        Assertions.assertDoesNotThrow(()-> new AHCSinkTask());
+    }
+
+    @Test
+    void test_constructor_with_mock_producer(){
+        MockProducer<String,Object> mockProducer = new MockProducer<>();
+        Assertions.assertDoesNotThrow(()-> new AHCSinkTask(mockProducer));
+    }
+}
