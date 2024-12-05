336e7d054d53695caa2c906071601c37873becf2
Thomas Lescot <184748260+Tlescot@users.noreply.github.com>
2024-12-05 02:42:58 +0100

    fix test

--- kafka-connect-http-core/src/test/java/io/github/clescot/kafka/connect/http/core/HttpRequestTest.java
+++ kafka-connect-http-core/src/test/java/io/github/clescot/kafka/connect/http/core/HttpRequestTest.java
@@ -1,15 +1,10 @@
 package io.github.clescot.kafka.connect.http.core;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.kjetland.jackson.jsonSchema.JsonSchemaConfig;
-import com.kjetland.jackson.jsonSchema.JsonSchemaDraft;
-import com.kjetland.jackson.jsonSchema.JsonSchemaGenerator;
 import io.confluent.connect.json.JsonSchemaConverter;
 import io.confluent.connect.json.JsonSchemaConverterConfig;
 import io.confluent.kafka.schemaregistry.ParsedSchema;
@@ -263,63 +258,7 @@ class HttpRequestTest {
         assertThat(deserializedHttpRequest).isEqualTo(httpRequest);
     }
 
-    @Test
-    void test_serialize_http_request_with_serializer_and_deserialize_with_high_level_converter() throws IOException {
-        //given
 
-        //build httpRequest
-        HttpRequest httpRequest = new HttpRequest(
-                "http://www.stuff.com",
-                HttpRequest.Method.GET,
-                "STRING"
-        );
-        httpRequest.setBodyAsString(DUMMY_BODY_AS_STRING);
-        Map<String, List<String>> headers = Maps.newHashMap();
-        headers.put("X-stuff", Lists.newArrayList("m-y-value"));
-        headers.put("X-correlation-id", Lists.newArrayList("44-999-33-dd"));
-        headers.put("X-request-id", Lists.newArrayList("11-999-ff-777"));
-        httpRequest.setHeaders(headers);
-
-        SpecificationVersion jsonSchemaSpecification = SpecificationVersion.DRAFT_2019_09;
-        boolean useOneOfForNullables=false;
-        boolean failUnknownProperties=false;
-        //get JSON schema
-        JsonSchema expectedJsonSchema = JsonSchemaUtils.getSchema(
-                httpRequest,
-                jsonSchemaSpecification,
-                useOneOfForNullables,
-                failUnknownProperties,
-                null
-        );
-
-        Map<String,String> jsonSchemaSerializerConfig = Maps.newHashMap();
-        jsonSchemaSerializerConfig.put(KafkaJsonSchemaSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG,"mock://stuff.com");
-        jsonSchemaSerializerConfig.put(KafkaJsonSchemaSerializerConfig.SCHEMA_SPEC_VERSION,jsonSchemaSpecification.toString());
-        jsonSchemaSerializerConfig.put(KafkaJsonSchemaSerializerConfig.WRITE_DATES_AS_ISO8601,"true");
-        jsonSchemaSerializerConfig.put(KafkaJsonSchemaSerializerConfig.ONEOF_FOR_NULLABLES,""+useOneOfForNullables);
-        jsonSchemaSerializerConfig.put(KafkaJsonSchemaSerializerConfig.FAIL_UNKNOWN_PROPERTIES,""+failUnknownProperties);
-        MockSchemaRegistryClient schemaRegistryClient = new MockSchemaRegistryClient(Lists.newArrayList(new JsonSchemaProvider()));
-        KafkaJsonSchemaSerializer<HttpRequest> serializer = new KafkaJsonSchemaSerializer<>(schemaRegistryClient,jsonSchemaSerializerConfig);
-
-        //serialize http as byte[]
-        byte[] bytes = serializer.serialize(DUMMY_TOPIC, httpRequest);
-
-        System.out.println("bytesAsString:"+new String(bytes, StandardCharsets.UTF_8));
-        JsonSchemaConverter jsonSchemaConverter = new JsonSchemaConverter(schemaRegistryClient);
-        Map<String,String> converterConfig= Maps.newHashMap();
-        converterConfig.put(JsonSchemaConverterConfig.SCHEMA_REGISTRY_URL_CONFIG,"mock://stuff.com");
-        converterConfig.put(KafkaJsonSchemaDeserializerConfig.JSON_VALUE_TYPE,HttpRequest.class.getName());
-        jsonSchemaConverter.configure(converterConfig,false);
-
-        //like in kafka connect Sink connector, convert byte[] to struct
-        SchemaAndValue schemaAndValue = jsonSchemaConverter.toConnectData(DUMMY_TOPIC, bytes);
-        Struct value = (Struct) schemaAndValue.value();
-        assertThat(expectedJsonSchema.equals(value.schema()));
-        //when
-        HttpRequest parsedHttpRequest = HttpRequestAsStruct.Builder.anHttpRequest().withStruct(value).build();
-
-        System.out.println(parsedHttpRequest);
-    }
 
 
     @Test
@@ -398,7 +337,7 @@ class HttpRequestTest {
         //build serializer
         Map<String,String> jsonSchemaDeSerializerConfig = Maps.newHashMap();
         jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG,"mock://stuff.com");
-        jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.JSON_VALUE_TYPE,HttpRequest.class.getClass().getName());
+        jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.JSON_VALUE_TYPE,HttpRequest.class.getName());
         jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.FAIL_UNKNOWN_PROPERTIES,""+failUnknownProperties);
         KafkaJsonSchemaDeserializer<HttpRequest> deserializer = new KafkaJsonSchemaDeserializer<>(schemaRegistryClient,jsonSchemaDeSerializerConfig,HttpRequest.class);
 
@@ -489,7 +428,7 @@ class HttpRequestTest {
         //build serializer
         Map<String,String> jsonSchemaDeSerializerConfig = Maps.newHashMap();
         jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG,"mock://stuff.com");
-        jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.JSON_VALUE_TYPE,HttpRequest.class.getClass().getName());
+        jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.JSON_VALUE_TYPE,HttpRequest.class.getName());
         jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.FAIL_UNKNOWN_PROPERTIES,""+failUnknownProperties);
         KafkaJsonSchemaDeserializer<HttpRequest> deserializer = new KafkaJsonSchemaDeserializer<>(schemaRegistryClient,jsonSchemaDeSerializerConfig,HttpRequest.class);
 
@@ -546,7 +485,7 @@ class HttpRequestTest {
         //build serializer
         Map<String,String> jsonSchemaDeSerializerConfig = Maps.newHashMap();
         jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG,"mock://stuff.com");
-        jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.JSON_VALUE_TYPE,HttpRequest.class.getClass().getName());
+        jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.JSON_VALUE_TYPE,HttpRequest.class.getName());
         jsonSchemaDeSerializerConfig.put(KafkaJsonSchemaDeserializerConfig.FAIL_UNKNOWN_PROPERTIES,""+failUnknownProperties);
         KafkaJsonSchemaDeserializer<HttpRequest> deserializer = new KafkaJsonSchemaDeserializer<>(schemaRegistryClient,jsonSchemaDeSerializerConfig,HttpRequest.class);
 
@@ -640,45 +579,4 @@ class HttpRequestTest {
         assertThat(parsedSchema).isNotPresent();
     }
 
-    @Test
-    void get_http_request_jsonschema(){
-        JsonSchemaConfig jsonSchemaConfig = getConfig(false, false);
-        jsonSchemaConfig = jsonSchemaConfig.withJsonSchemaDraft(JsonSchemaDraft.DRAFT_2019_09);
-        ObjectMapper objectMapper = new JsonMapper();
-        JsonSchemaGenerator jsonSchemaGenerator = new JsonSchemaGenerator(objectMapper, jsonSchemaConfig);
-        JsonNode jsonSchema = jsonSchemaGenerator.generateJsonSchema(HttpRequest.class);
-        JsonSchema schema = new JsonSchema(jsonSchema);
-        String schemaAsString = schema.toString();
-        assertThat(schemaAsString).isNotNull();
-    }
-
-    /**
-     * from JsonSchemaUtils...
-     * @param useOneofForNullables
-     * @param failUnknownProperties
-     * @return
-     */
-    private static JsonSchemaConfig getConfig(
-            boolean useOneofForNullables, boolean failUnknownProperties) {
-        final JsonSchemaConfig vanilla = JsonSchemaConfig.vanillaJsonSchemaDraft4();
-        return JsonSchemaConfig.create(
-                vanilla.autoGenerateTitleForProperties(),
-                Optional.empty(),
-                true,
-                useOneofForNullables,
-                vanilla.usePropertyOrdering(),
-                vanilla.hidePolymorphismTypeProperty(),
-                vanilla.disableWarnings(),
-                vanilla.useMinLengthForNotNull(),
-                vanilla.useTypeIdForDefinitionName(),
-                Collections.emptyMap(),
-                vanilla.useMultipleEditorSelectViaProperty(),
-                Collections.emptySet(),
-                Collections.emptyMap(),
-                Collections.emptyMap(),
-                vanilla.subclassesResolver(),
-                failUnknownProperties,
-                null
-        );
-    }
 }
