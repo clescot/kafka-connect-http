1f891aedf26a7c23bd21a81c073eb8f9af0931ca
Thomas Lescot <184748260+Tlescot@users.noreply.github.com>
2025-01-15 01:35:39 +0100

    add tests.

--- kafka-connect-http-core/src/test/java/io/github/clescot/kafka/connect/http/core/HttpPartTest.java
+++ kafka-connect-http-core/src/test/java/io/github/clescot/kafka/connect/http/core/HttpPartTest.java
@@ -1,5 +1,6 @@
 package io.github.clescot.kafka.connect.http.core;
 
+import java.net.URL;
 import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class HttpPartTest {
+
+    @Nested
+    class TestConstructor{
+
+        @Test
+        void test_constructor_with_byte_array(){
+            assertDoesNotThrow(()->new HttpPart("test".getBytes(StandardCharsets.UTF_8)));
+        }
+        @Test
+        void test_constructor_with_byte_array_and_headers(){
+            Map<String,List<String>> headers = new HashMap<>();
+            headers.put("Content-Type",List.of("application/json"));
+            assertDoesNotThrow(()->new HttpPart(headers,"test".getBytes(StandardCharsets.UTF_8)));
+        }
+        @Test
+        void test_constructor_with_string_content(){
+            assertDoesNotThrow(()->new HttpPart("test"));
+        }
+        @Test
+        void test_constructor_with_string_content_and_headers(){
+            Map<String,List<String>> headers = new HashMap<>();
+            headers.put("Content-Type",List.of("application/json"));
+            assertDoesNotThrow(()->new HttpPart(headers,"test"));
+        }
+        @Test
+        void test_constructor_with_file_as_ref_content(){
+            URL url = Thread.currentThread().getContextClassLoader().getResource("upload.txt");
+            assertDoesNotThrow(()->new HttpPart("parameterName","parameterValue",url.toURI()));
+        }
+        @Test
+        void test_constructor_with_file_as_ref_content_and_headers(){
+            Map<String,List<String>> headers = new HashMap<>();
+            headers.put("Content-Type",List.of("application/json"));
+            URL url = Thread.currentThread().getContextClassLoader().getResource("upload.txt");
+            assertDoesNotThrow(()->new HttpPart(headers,"parameterName","parameterValue",url.toURI()));
+        }
+    }
+
 
     @Nested
     class TestEquals{
