90e83e34ecab3c3b05bedd4b38c489883c39583f
Charles Lescot <910188+clescot@users.noreply.github.com>
2025-07-10 00:19:01 +0200

    fix skip test when publish #8

--- kafka-connect-http-core/src/main/java/io/github/clescot/kafka/connect/http/core/SchemaLoader.java
+++ kafka-connect-http-core/src/main/java/io/github/clescot/kafka/connect/http/core/SchemaLoader.java
@@ -17,7 +17,7 @@ public class SchemaLoader {
     private static final Logger LOGGER = LoggerFactory.getLogger(SchemaLoader.class);
     private static String loadSchema(String schemaPath) throws IOException, URISyntaxException {
         try {
-            URL url = Thread.currentThread().getContextClassLoader().getResource(schemaPath);
+            URL url = SchemaLoader.class.getResource(schemaPath);
             if (url == null) {
                 LOGGER.error("Schema file not found: " + schemaPath);
                 throw new IllegalStateException("Schema file not found: " + schemaPath);
@@ -53,14 +53,14 @@ public class SchemaLoader {
 
     public static ParsedSchema loadHttpRequestSchema(){
         try {
-            return  new JsonSchema(loadSchema("schemas/json/versions/3/http-request.json"));
+            return  new JsonSchema(loadSchema("/schemas/json/versions/3/http-request.json"));
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
     }
     public static ParsedSchema loadHttpResponseSchema(){
         try {
-            return new JsonSchema(loadSchema("schemas/json/versions/3/http-response.json"));
+            return new JsonSchema(loadSchema("/schemas/json/versions/3/http-response.json"));
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
@@ -68,14 +68,14 @@ public class SchemaLoader {
 
     public static ParsedSchema loadHttpPartSchema(){
         try {
-            return  new JsonSchema(loadSchema("schemas/json/versions/3/http-part.json"));
+            return  new JsonSchema(loadSchema("/schemas/json/versions/3/http-part.json"));
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
     }
     public static ParsedSchema loadHttpExchangeSchema(){
         try {
-            return  new JsonSchema(loadSchema("schemas/json/versions/3/http-exchange.json"));
+            return  new JsonSchema(loadSchema("/schemas/json/versions/3/http-exchange.json"));
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
