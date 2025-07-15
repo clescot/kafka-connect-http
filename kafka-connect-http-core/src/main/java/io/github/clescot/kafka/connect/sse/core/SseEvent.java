8c79cce61e4b938cf925556f3fc0163873cf0111
Thomas Lescot <184748260+Tlescot@users.noreply.github.com>
2025-07-15 18:11:56 +0200

    add javadoc.

--- kafka-connect-http-core/src/main/java/io/github/clescot/kafka/connect/sse/core/SseEvent.java
+++ kafka-connect-http-core/src/main/java/io/github/clescot/kafka/connect/sse/core/SseEvent.java
@@ -3,6 +3,10 @@ package io.github.clescot.kafka.connect.sse.core;
 import java.io.Serial;
 import java.io.Serializable;
 
+/**
+ * Represents a Server-Sent Event (SSE) with an ID, type, and data.
+ * This class is serializable and can be cloned.
+ */
 public class SseEvent implements Cloneable, Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
