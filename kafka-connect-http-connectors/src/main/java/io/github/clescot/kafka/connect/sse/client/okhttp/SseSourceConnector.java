262eee314ade95ae394a6b3c141070fb06704516
Thomas Lescot <184748260+Tlescot@users.noreply.github.com>
2025-07-16 17:40:04 +0200

    init SseSourceConnector.java

--- kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/sse/client/okhttp/SseSourceConnector.java
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/sse/client/okhttp/SseSourceConnector.java
@@ -1,20 +1,23 @@
 package io.github.clescot.kafka.connect.sse.client.okhttp;
 
+import com.google.common.base.Preconditions;
 import io.github.clescot.kafka.connect.http.VersionUtils;
+import io.github.clescot.kafka.connect.http.source.cron.HttpCronSourceConnectorConfig;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.Task;
 import org.apache.kafka.connect.source.SourceConnector;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class SseSourceConnector extends SourceConnector {
 
     private static final VersionUtils VERSION_UTILS = new VersionUtils();
-
+    private SseSourceConnectorConfig sseSourceConnectorConfig;
     @Override
-    public void start(Map<String, String> map) {
-
+    public void start(Map<String, String> props) {
+        this.sseSourceConnectorConfig = new SseSourceConnectorConfig(config(),props);
     }
 
     @Override
@@ -23,8 +26,14 @@ public class SseSourceConnector extends SourceConnector {
     }
 
     @Override
-    public List<Map<String, String>> taskConfigs(int i) {
-        return List.of();
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+        Preconditions.checkArgument(maxTasks>0,"maxTasks must be higher than 0");
+        List<Map<String, String>> configs = new ArrayList<>(maxTasks);
+        for (int i = 0; i < maxTasks; i++) {
+            configs.add(this.sseSourceConnectorConfig.originalsStrings());
+        }
+
+        return configs;
     }
 
     @Override
@@ -34,11 +43,11 @@ public class SseSourceConnector extends SourceConnector {
 
     @Override
     public ConfigDef config() {
-        return null;
+        return SseSourceConfigDefinition.config();
     }
 
     @Override
     public String version() {
-        return "";
+        return VERSION_UTILS.getVersion();
     }
 }
