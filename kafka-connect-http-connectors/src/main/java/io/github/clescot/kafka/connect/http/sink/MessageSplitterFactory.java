3dcd45c0fc0da76a8824c8ab027dba84b04b6b57
Thomas Lescot <184748260+Tlescot@users.noreply.github.com>
2025-07-20 17:06:33 +0200

    refacto without kafka connect dependency.

--- kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/sink/MessageSplitterFactory.java
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/http/sink/MessageSplitterFactory.java
@@ -2,20 +2,19 @@ package io.github.clescot.kafka.connect.http.sink;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import io.github.clescot.kafka.connect.http.MapUtils;
 import org.apache.commons.jexl3.JexlEngine;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static io.github.clescot.kafka.connect.http.sink.HttpSinkConfigDefinition.MESSAGE_SPLITTER_IDS;
-
 public class MessageSplitterFactory {
     public static final String MESSAGE_SPLITTER = "message.splitter.";
-    public List<MessageSplitter> buildMessageSplitters(HttpSinkConnectorConfig connectorConfig, JexlEngine jexlEngine) {
+    public List<MessageSplitter> buildMessageSplitters(Map<String, Object> config, JexlEngine jexlEngine, List<String> messageSplitterIds) {
         List<MessageSplitter> requestSplitterList = Lists.newArrayList();
-        for (String splitterId : Optional.ofNullable(connectorConfig.getList(MESSAGE_SPLITTER_IDS)).orElse(Lists.newArrayList())) {
-            Map<String, Object> settings = connectorConfig.originalsWithPrefix(MESSAGE_SPLITTER + splitterId + ".");
+        for (String splitterId : Optional.ofNullable(messageSplitterIds).orElse(Lists.newArrayList())) {
+            Map<String, Object> settings = MapUtils.getMapWithPrefix(config,MESSAGE_SPLITTER + splitterId + ".");
             String splitPattern = (String) settings.get("pattern");
             Preconditions.checkNotNull(splitPattern,"message splitter '"+splitterId+"' splitPattern is required");
             String limit = (String) settings.get("limit");
