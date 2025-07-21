0130fa5ed4dff9ab58ceed6ad52113ff7b697867
Thomas Lescot <184748260+Tlescot@users.noreply.github.com>
2025-07-21 22:08:35 +0200

    remove unused import

--- kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/sse/client/okhttp/SseTask.java
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/sse/client/okhttp/SseTask.java
@@ -1,7 +1,6 @@
 package io.github.clescot.kafka.connect.sse.client.okhttp;
 
 import io.github.clescot.kafka.connect.http.Task;
-import io.github.clescot.kafka.connect.http.client.Configuration;
 import io.github.clescot.kafka.connect.http.client.okhttp.OkHttpClient;
 import io.github.clescot.kafka.connect.http.core.HttpRequest;
 import io.github.clescot.kafka.connect.sse.core.SseEvent;
