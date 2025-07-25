963a765255cb1b8b11c5da6a47717066d690468c
Thomas Lescot <184748260+Tlescot@users.noreply.github.com>
2025-07-25 18:51:48 +0200

    add parameters for SseConfigDefinition

--- kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/sse/client/okhttp/SseConfigDefinition.java
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/sse/client/okhttp/SseConfigDefinition.java
@@ -1,6 +1,7 @@
 package io.github.clescot.kafka.connect.sse.client.okhttp;
 
 import com.google.common.collect.Sets;
+import com.launchdarkly.eventsource.ErrorStrategy;
 import io.github.clescot.kafka.connect.ConfigUtils;
 import io.github.clescot.kafka.connect.http.client.HttpClientConfigDefinition;
 import io.github.clescot.kafka.connect.http.sink.SinkConfigDefinition;
@@ -18,8 +19,25 @@ public class SseConfigDefinition {
     public static final String DEFAULT_CONFIG_URL = DEFAULT_CONFIGURATION_PREFIX+URL;
     public static final String URL_DOC = "URL of the SSE server to connect to";
     public static final String TOPIC = "topic";
+
     public static final String DEFAULT_CONFIG_TOPIC = DEFAULT_CONFIGURATION_PREFIX+"topic";
     public static final String TOPIC_DOC = "topic to publish events to";
+
+    //error strategy
+    public static final String ERROR_STRATEGY = "error.strategy";
+    public static final String ERROR_STRATEGY_ALWAYS_CONTINUE = "always-continue";
+    public static final String ERROR_STRATEGY_ALWAYS_THROW = "always-throw";
+    public static final String ERROR_STRATEGY_CONTINUE_WITH_MAX_ATTEMPTS = "continue-with-max-attempts";
+    public static final String ERROR_STRATEGY_MAX_ATTEMPTS = "max-attempts";
+    public static final String ERROR_STRATEGY_TIME_LIMIT_COUNT_IN_MILLIS = "time-limit-count-in-millis";
+    public static final String ERROR_STRATEGY_CONTINUE_WITH_TIME_LIMIT = "continue-with-time-limit";
+    public static final String RETRY_DELAY_STRATEGY_MAX_DELAY_MILLIS = "retry.delay.strategy.max-delay-millis";
+    public static final String RETRY_DELAY_STRATEGY_BACKOFF_MULTIPLIER = "retry.delay.strategy.backoff-multiplier";
+    public static final String RETRY_DELAY_STRATEGY_JITTER_MULTIPLIER = "retry.delay.strategy.jitter-multiplier";
+
+    //retry delay strategy
+    public static final String RETRY_DELAY_STRATEGY = "retry.delay.strategy";
+
     private final Map<String, String> settings;
 
     public SseConfigDefinition(Map<String, String> settings) {
@@ -51,6 +69,8 @@ public class SseConfigDefinition {
         String prefix = "config." + configurationName + ".";
         return configDef
                 .define(prefix + URL, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, URL_DOC)
-                .define(prefix + TOPIC, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, TOPIC_DOC);
+                .define(prefix + TOPIC, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, TOPIC_DOC)
+                .define(prefix + ERROR_STRATEGY, ConfigDef.Type.STRING,ERROR_STRATEGY_ALWAYS_CONTINUE, ConfigDef.Importance.MEDIUM, TOPIC_DOC)
+                ;
     }
 }
