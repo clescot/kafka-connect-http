d33ed17db304921c5a50e3c5c9a9016e15b5f195
Thomas Lescot <184748260+Tlescot@users.noreply.github.com>
2025-07-28 14:19:41 +0200

    add tests.

--- kafka-connect-http-connectors/src/test/java/io/github/clescot/kafka/connect/sse/client/okhttp/SseConfigurationTest.java
+++ kafka-connect-http-connectors/src/test/java/io/github/clescot/kafka/connect/sse/client/okhttp/SseConfigurationTest.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Maps;
 import io.github.clescot.kafka.connect.http.client.HttpClientConfiguration;
 import io.github.clescot.kafka.connect.http.client.okhttp.OkHttpClient;
 import io.github.clescot.kafka.connect.http.client.okhttp.OkHttpClientFactory;
+import io.github.clescot.kafka.connect.http.core.queue.QueueFactory;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -70,6 +71,67 @@ class SseConfigurationTest {
             );
             HashMap<String, Object> emptySettings = Maps.newHashMap();
             Assertions.assertThrows(IllegalArgumentException.class,()->new SseConfiguration("test",configuration, emptySettings));
+        }
+    }
+
+    @Nested
+    class BuildSseConfiguration{
+        @Test
+        void nominal_case() {
+            Map<String, Object> settings = Maps.newHashMap();
+            settings.put("url", "http://example.com/sse");
+            settings.put("topic", "test-topic");
+            SseConfiguration sseConfiguration = SseConfiguration.buildSseConfiguration("test-id", settings);
+            assertThat(sseConfiguration.getConfigurationId()).isEqualTo("test-id");
+            assertThat(sseConfiguration.getUri().toString()).hasToString("http://example.com/sse");
+            assertThat(sseConfiguration.getTopic()).isEqualTo("test-topic");
+        }
+
+        @Test
+        void null_settings() {
+            Assertions.assertThrows(NullPointerException.class, () -> SseConfiguration.buildSseConfiguration("test-id", null));
+        }
+
+        @Test
+        void empty_settings() {
+            HashMap<String, Object> emptySettings = Maps.newHashMap();
+            Assertions.assertThrows(IllegalArgumentException.class, () -> SseConfiguration.buildSseConfiguration("test-id", emptySettings));
+        }
+    }
+
+    @Nested
+    class Connect{
+        @Test
+        void nominal_case() {
+            HttpClientConfiguration<OkHttpClient, Request, Response> configuration = new HttpClientConfiguration<>(
+                    "test-id",
+                    new OkHttpClientFactory(),
+                    Map.of("url", "http://example.com/sse", "topic", "test-topic"),
+                    null,
+                    new CompositeMeterRegistry()
+            );
+            HashMap<String, Object> settings = Maps.newHashMap();
+            settings.put("url", "http://example.com/sse");
+            settings.put("topic", "test-topic");
+            SseConfiguration sseConfiguration = new SseConfiguration("test-id", configuration, settings);
+            sseConfiguration.connect(QueueFactory.getQueue(QueueFactory.DEFAULT_QUEUE_NAME));
+            assertThat(sseConfiguration.getQueue()).isNotNull();
+        }
+
+        @Test
+        void null_queue() {
+            HttpClientConfiguration<OkHttpClient, Request, Response> configuration = new HttpClientConfiguration<>(
+                    "test-id",
+                    new OkHttpClientFactory(),
+                    Map.of("url", "http://example.com/sse", "topic", "test-topic"),
+                    null,
+                    new CompositeMeterRegistry()
+            );
+            HashMap<String, Object> settings = Maps.newHashMap();
+            settings.put("url", "http://example.com/sse");
+            settings.put("topic", "test-topic");
+            SseConfiguration sseConfiguration = new SseConfiguration("test-id", configuration, settings);
+            Assertions.assertThrows(NullPointerException.class, () -> sseConfiguration.connect(null));
         }
     }
 
