f35dca87d4ef761050a18216dbeb6c64d61fc46a
Thomas Lescot <184748260+Tlescot@users.noreply.github.com>
2025-07-30 19:12:37 +0200

    add filterEntriesStartingWithPrefixes to MapUtils

--- kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/MapUtils.java
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/MapUtils.java
@@ -5,6 +5,18 @@ import java.util.stream.Collectors;
 
 public class MapUtils {
 
+
+    private MapUtils() {
+        // Utility class, no instantiation
+    }
+
+    /**
+     * Returns a new map containing only the entries from the original map
+     * whose keys start with the specified prefix. The prefix is removed from the keys.
+     * @param map
+     * @param prefix
+     * @return
+     */
     public static Map<String,Object> getMapWithPrefix(Map<String, Object> map, String prefix) {
         return map.entrySet().stream()
                 .filter(entry -> entry.getKey().startsWith(prefix))
@@ -13,4 +25,22 @@ public class MapUtils {
                         Map.Entry::getValue
                 ));
     }
+
+
+
+    public static <T> Map<String,T> filterEntriesStartingWithPrefixes(Map<String, T> map, String... prefixes) {
+        return map.entrySet().stream()
+                .filter(entry -> {
+                    for (String prefix : prefixes) {
+                        if (entry.getKey().startsWith(prefix)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue)
+                );
+    }
+
+
 }
