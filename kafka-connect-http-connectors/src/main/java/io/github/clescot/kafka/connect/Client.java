409ea07491571baaf3673dfd8b6e7434355ffbc3
charles lescot <910188+clescot@users.noreply.github.com>
2025-12-24 14:52:25 +0100

    Fix sonar issues (#900)
    * optimize imports
    
    * fix minor issues
    
    * add vscode settings to .gitignore
    
    ---------
    
    Co-authored-by: Camille Lescot <183648426+CamilleLsct@users.noreply.github.com>

--- kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/Client.java
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/Client.java
@@ -1,9 +1,5 @@
 package io.github.clescot.kafka.connect;
 
-import dev.failsafe.RateLimiter;
-
-import java.util.Optional;
-
 public interface Client<E> {
 
 
--- kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/RequestClient.java
+++ kafka-connect-http-connectors/src/main/java/io/github/clescot/kafka/connect/RequestClient.java
@@ -19,19 +19,20 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.github.clescot.kafka.connect.http.sink.HttpConfigDefinition.*;
-import static io.github.clescot.kafka.connect.http.sink.HttpConfigDefinition.RATE_LIMITER_SCOPE;
 
 /**
- *  A client interface that handles requests and native requests.
- * @param <R> request
+ * A client interface that handles requests and native requests.
+ * 
+ * @param <R>  request
  * @param <NR> native request
  */
 @SuppressWarnings("java:S119")
-public interface RequestClient<R extends Request,NR,E> extends Client<E>{
+public interface RequestClient<R extends Request, NR, E> extends Client<E> {
     String STATIC_SCOPE = "static";
     Map<String, RateLimiter> SHARED_RATE_LIMITERS = Maps.newHashMap();
     Logger LOGGER = LoggerFactory.getLogger(RequestClient.class);
     int ONE_REQUEST = 1;
+
     /**
      * convert an Request into a native request.
      *
@@ -40,11 +41,11 @@ public interface RequestClient<R extends Request,NR,E> extends Client<E>{
      */
     NR buildNativeRequest(R request);
 
-
     R buildRequest(NR nativeRequest);
 
     /**
      * raw native HttpRequest call.
+     * 
      * @param request native HttpRequest
      * @return Void or a CompletableFuture of a native HttpResponse.
      */
@@ -58,9 +59,6 @@ public interface RequestClient<R extends Request,NR,E> extends Client<E>{
 
     String getPermitsPerExecution();
 
-
-
-
     default Stopwatch rateLimitCall(R request) throws InterruptedException {
         Optional<RateLimiter<E>> limiter = getRateLimiter();
         Stopwatch rateLimitedStopWatch = null;
@@ -71,10 +69,10 @@ public interface RequestClient<R extends Request,NR,E> extends Client<E>{
             if (RATE_LIMITER_REQUEST_LENGTH_PER_CALL.equals(permitsPerExecution)) {
                 long length = request.getLength();
                 httpExchangeRateLimiter.acquirePermits(Math.toIntExact(length));
-                LOGGER.warn("{} permits acquired for request:'{}'", length, request);
+                LOGGER.trace("{} permits acquired for request:'{}'", length, request);
             } else {
                 httpExchangeRateLimiter.acquirePermits(ONE_REQUEST);
-                LOGGER.warn("1 permit acquired for request:'{}'", request);
+                LOGGER.trace("1 permit acquired for request:'{}'", request);
             }
         } else {
             LOGGER.trace("no rate limiter is configured");
@@ -87,29 +85,34 @@ public interface RequestClient<R extends Request,NR,E> extends Client<E>{
         if (configMap.containsKey(RATE_LIMITER_MAX_EXECUTIONS)) {
             long maxExecutions = Long.parseLong(configMap.get(RATE_LIMITER_MAX_EXECUTIONS));
             LOGGER.trace("configuration '{}' : maxExecutions :{}", configurationId, maxExecutions);
-            long periodInMs = Long.parseLong(Optional.ofNullable(configMap.get(RATE_LIMITER_PERIOD_IN_MS)).orElse(1000 + ""));
+            long periodInMs = Long
+                    .parseLong(Optional.ofNullable(configMap.get(RATE_LIMITER_PERIOD_IN_MS)).orElse(1000 + ""));
             LOGGER.trace("configuration '{}' : periodInMs :{}", configurationId, periodInMs);
-            if (configMap.containsKey(RATE_LIMITER_SCOPE) && STATIC_SCOPE.equalsIgnoreCase(configMap.get(RATE_LIMITER_SCOPE))) {
+            if (configMap.containsKey(RATE_LIMITER_SCOPE)
+                    && STATIC_SCOPE.equalsIgnoreCase(configMap.get(RATE_LIMITER_SCOPE))) {
                 LOGGER.trace("configuration '{}' : rateLimiter scope is 'static'", configurationId);
-                Optional<RateLimiter<E>> sharedRateLimiter = Optional.ofNullable(SHARED_RATE_LIMITERS.get(configurationId));
+                Optional<RateLimiter<E>> sharedRateLimiter = Optional
+                        .ofNullable(SHARED_RATE_LIMITERS.get(configurationId));
                 if (sharedRateLimiter.isPresent()) {
                     myRateLimiter = sharedRateLimiter.get();
                     LOGGER.trace("configuration '{}' : rate limiter is already shared", configurationId);
                 } else {
-                    myRateLimiter = RateLimiter.<E>smoothBuilder(maxExecutions, Duration.of(periodInMs, ChronoUnit.MILLIS)).build();
+                    myRateLimiter = RateLimiter
+                            .<E>smoothBuilder(maxExecutions, Duration.of(periodInMs, ChronoUnit.MILLIS)).build();
                     registerRateLimiter(configurationId, myRateLimiter);
                 }
             } else {
-                myRateLimiter = RateLimiter.<E>smoothBuilder(maxExecutions, Duration.of(periodInMs, ChronoUnit.MILLIS)).build();
+                myRateLimiter = RateLimiter.<E>smoothBuilder(maxExecutions, Duration.of(periodInMs, ChronoUnit.MILLIS))
+                        .build();
             }
             RateLimiterConfig<E> rateLimiterConfig = myRateLimiter.getConfig();
-            LOGGER.trace("configuration '{}' rate limiter configured : 'maxRate:{},maxPermits{},period:{},maxWaitTime:{}'",
+            LOGGER.trace(
+                    "configuration '{}' rate limiter configured : 'maxRate:{},maxPermits{},period:{},maxWaitTime:{}'",
                     configurationId,
                     rateLimiterConfig.getMaxRate(),
                     rateLimiterConfig.getMaxPermits(),
                     rateLimiterConfig.getPeriod(),
-                    rateLimiterConfig.getMaxWaitTime()
-            );
+                    rateLimiterConfig.getMaxWaitTime());
         } else {
             if (LOGGER.isTraceEnabled()) {
                 LOGGER.trace("configuration '{}' : rate limiter is not configured", configurationId);
@@ -121,10 +124,10 @@ public interface RequestClient<R extends Request,NR,E> extends Client<E>{
 
     private void registerRateLimiter(String configurationId, RateLimiter<E> rateLimiter) {
         Preconditions.checkNotNull(configurationId, "we cannot register a rateLimiter for a 'null' configurationId");
-        Preconditions.checkNotNull(rateLimiter, "we cannot register a 'null' rate limiter for the configurationId " + configurationId);
+        Preconditions.checkNotNull(rateLimiter,
+                "we cannot register a 'null' rate limiter for the configurationId " + configurationId);
         LOGGER.info("registration of a shared rateLimiter for the configurationId '{}'", configurationId);
         SHARED_RATE_LIMITERS.put(configurationId, rateLimiter);
     }
-
 
 }
